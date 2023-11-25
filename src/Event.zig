1042dfaea49c02f921194730408f56b1dd3a51e5
AnErrupTion <anerruption@disroot.org>
2023-11-25 15:59:30 +0100

    Split Event, RectScale, Widget, WidgetData
    Signed-off-by: AnErrupTion <anerruption@disroot.org>

---
+++ src/Event.zig
@@ -0,0 +1,106 @@
+const dvui = @import("dvui.zig");
+
+const Point = dvui.Point;
+const Rect = dvui.Rect;
+
+const enums = dvui.enums;
+
+const Event = @This();
+
+handled: bool = false,
+focus_windowId: ?u32 = null,
+focus_widgetId: ?u32 = null,
+// num increments withing a frame, used in focusRemainingEvents
+num: u16 = 0,
+evt: union(enum) {
+    // non-bubbleable
+    mouse: Mouse,
+
+    // bubbleable
+    key: Key,
+    text: []u8,
+    close_popup: ClosePopup,
+    scroll_drag: ScrollDrag,
+    scroll_to: ScrollTo,
+},
+
+// All widgets have to bubble keyboard events if they can have keyboard focus
+// so that pressing the up key in any child of a scrollarea will scroll.  Call
+// this helper at the end of processEvent().
+pub fn bubbleable(self: *const Event) bool {
+    return (!self.handled and (self.evt != .mouse));
+}
+
+pub const Key = struct {
+    code: enums.Key,
+    action: enum {
+        down,
+        repeat,
+        up,
+    },
+    mod: enums.Mod,
+};
+
+pub const Mouse = struct {
+    pub const Action = enum {
+        // Focus events come right before their associated pointer event, usually
+        // leftdown/rightdown or motion. Separated to enable changing what
+        // causes focus changes.
+        focus,
+        press,
+        release,
+
+        wheel_y,
+
+        // motion Point is the change in position
+        // if you just want to react to the current mouse position if it got
+        // moved at all, use the .position event with mouseTotalMotion()
+        motion,
+
+        // only one position event per frame, and it's always after all other
+        // mouse events, used to change mouse cursor and do widget highlighting
+        // - also useful with mouseTotalMotion() to respond to mouse motion but
+        // only at the final location
+        position,
+    };
+
+    action: Action,
+
+    // This distinguishes between mouse and touch events.
+    // .none is used for mouse motion, wheel, and position
+    button: enums.Button,
+
+    p: Point,
+    floating_win: u32,
+
+    data: union {
+        none: void,
+        motion: Point,
+        wheel_y: f32,
+    } = .{ .none = {} },
+};
+
+pub const ClosePopup = struct {
+    // are we closing because of a specific user action (clicked on menu item,
+    // pressed escape), or because they clicked off the menu somewhere?
+    intentional: bool = true,
+};
+
+pub const ScrollDrag = struct {
+    // bubbled up from a child to tell a containing scrollarea to
+    // possibly scroll to show more of the child
+    mouse_pt: Point,
+    screen_rect: Rect,
+    capture_id: u32,
+    injected: bool,
+};
+
+pub const ScrollTo = struct {
+    // bubbled up from a child to tell a containing scrollarea to
+    // scroll to show the given rect
+    screen_rect: Rect,
+
+    // whether to scroll outside the current scroll bounds (useful if the
+    // current action might be expanding the scroll area)
+    over_scroll: bool = false,
+};
---
+++ src/Widget.zig
@@ -0,0 +1,106 @@
+const std = @import("std");
+const dvui = @import("dvui.zig");
+
+const Event = dvui.Event;
+const Options = dvui.Options;
+const Rect = dvui.Rect;
+const RectScale = dvui.RectScale;
+const Size = dvui.Size;
+const WidgetData = dvui.WidgetData;
+
+const Widget = @This();
+
+ptr: *anyopaque,
+vtable: *const VTable,
+
+const VTable = struct {
+    data: *const fn (ptr: *anyopaque) *WidgetData,
+    rectFor: *const fn (ptr: *anyopaque, id: u32, min_size: Size, e: Options.Expand, g: Options.Gravity) Rect,
+    screenRectScale: *const fn (ptr: *anyopaque, r: Rect) RectScale,
+    minSizeForChild: *const fn (ptr: *anyopaque, s: Size) void,
+    processEvent: *const fn (ptr: *anyopaque, e: *Event, bubbling: bool) void,
+};
+
+pub fn init(
+    pointer: anytype,
+    comptime dataFn: fn (ptr: @TypeOf(pointer)) *WidgetData,
+    comptime rectForFn: fn (ptr: @TypeOf(pointer), id: u32, min_size: Size, e: Options.Expand, g: Options.Gravity) Rect,
+    comptime screenRectScaleFn: fn (ptr: @TypeOf(pointer), r: Rect) RectScale,
+    comptime minSizeForChildFn: fn (ptr: @TypeOf(pointer), s: Size) void,
+    comptime processEventFn: fn (ptr: @TypeOf(pointer), e: *Event, bubbling: bool) void,
+) Widget {
+    const Ptr = @TypeOf(pointer);
+    const ptr_info = @typeInfo(Ptr);
+    std.debug.assert(ptr_info == .Pointer); // Must be a pointer
+    std.debug.assert(ptr_info.Pointer.size == .One); // Must be a single-item pointer
+
+    const gen = struct {
+        fn dataImpl(ptr: *anyopaque) *WidgetData {
+            const self = @as(Ptr, @ptrCast(@alignCast(ptr)));
+            return @call(.always_inline, dataFn, .{self});
+        }
+
+        fn rectForImpl(ptr: *anyopaque, id: u32, min_size: Size, e: Options.Expand, g: Options.Gravity) Rect {
+            const self = @as(Ptr, @ptrCast(@alignCast(ptr)));
+            return @call(.always_inline, rectForFn, .{ self, id, min_size, e, g });
+        }
+
+        fn screenRectScaleImpl(ptr: *anyopaque, r: Rect) RectScale {
+            const self = @as(Ptr, @ptrCast(@alignCast(ptr)));
+            return @call(.always_inline, screenRectScaleFn, .{ self, r });
+        }
+
+        fn minSizeForChildImpl(ptr: *anyopaque, s: Size) void {
+            const self = @as(Ptr, @ptrCast(@alignCast(ptr)));
+            return @call(.always_inline, minSizeForChildFn, .{ self, s });
+        }
+
+        fn processEventImpl(ptr: *anyopaque, e: *Event, bubbling: bool) void {
+            const self = @as(Ptr, @ptrCast(@alignCast(ptr)));
+            return @call(.always_inline, processEventFn, .{ self, e, bubbling });
+        }
+
+        const vtable = VTable{
+            .data = dataImpl,
+            .rectFor = rectForImpl,
+            .screenRectScale = screenRectScaleImpl,
+            .minSizeForChild = minSizeForChildImpl,
+            .processEvent = processEventImpl,
+        };
+    };
+
+    return .{
+        .ptr = pointer,
+        .vtable = &gen.vtable,
+    };
+}
+
+pub fn data(self: Widget) *WidgetData {
+    return self.vtable.data(self.ptr);
+}
+
+pub fn extendId(self: Widget, src: std.builtin.SourceLocation, id_extra: usize) u32 {
+    var hash = dvui.fnv.init();
+    hash.value = self.data().id;
+    hash.update(src.file);
+    hash.update(std.mem.asBytes(&src.line));
+    hash.update(std.mem.asBytes(&src.column));
+    hash.update(std.mem.asBytes(&id_extra));
+    return hash.final();
+}
+
+pub fn rectFor(self: Widget, id: u32, min_size: Size, e: Options.Expand, g: Options.Gravity) Rect {
+    return self.vtable.rectFor(self.ptr, id, min_size, e, g);
+}
+
+pub fn screenRectScale(self: Widget, r: Rect) RectScale {
+    return self.vtable.screenRectScale(self.ptr, r);
+}
+
+pub fn minSizeForChild(self: Widget, s: Size) void {
+    self.vtable.minSizeForChild(self.ptr, s);
+}
+
+pub fn processEvent(self: Widget, e: *Event, bubbling: bool) void {
+    self.vtable.processEvent(self.ptr, e, bubbling);
+}
