43f4fb898c511c916ebc56c768863478b46e5987
David Vanderson <david.vanderson@gmail.com>
2024-02-02 20:32:18 -0500

    web-test: use standalone template

--- web-test.zig
+++ web-test.zig
@@ -69,32 +69,15 @@ export fn app_update() i32 {
 }
 
 fn update() !i32 {
-    // beginWait is not necessary, but cooperates with waitTime to properly
-    // wait for timers/animations.
     var nstime = win.beginWait(backend.hasEvent());
 
     try win.begin(nstime);
-    backend.clear();
 
     try backend.addAllEvents(&win);
 
-    _ = try dvui.Examples.demo();
+    backend.clear();
 
-    var box = try dvui.box(@src(), .vertical, .{ .background = true, .color_fill = .{ .color = .{ .b = 0, .g = 0 } } });
-    try dvui.label(@src(), "hello", .{}, .{ .gravity_x = 0.5 });
-
-    if (try dvui.button(@src(), "Show Demo Window", .{}, .{})) {
-        dvui.Examples.show_demo_window = !dvui.Examples.show_demo_window;
-    }
-
-    var buf: [100]u8 = undefined;
-    const fps_str = std.fmt.bufPrint(&buf, "{d:0>4.0} fps", .{dvui.FPS()}) catch unreachable;
-    try dvui.label(@src(), "{s}", .{fps_str}, .{ .gravity_x = 0.5 });
-
-    try dvui.label(@src(), "nanoTime {d}", .{dvui.currentWindow().frame_time_ns}, .{ .gravity_x = 0.5 });
-
-    //try dvui.debugFontAtlases(@src(), .{});
-    box.deinit();
+    try dvui_frame();
 
     const end_micros = try win.end(.{});
 
@@ -107,4 +90,63 @@ fn update() !i32 {
 
     const wait_event_micros = win.waitTime(end_micros, null);
     return @intCast(@divTrunc(wait_event_micros, 1000));
+}
+
+fn dvui_frame() !void {
+    {
+        var m = try dvui.menu(@src(), .horizontal, .{ .background = true, .expand = .horizontal });
+        defer m.deinit();
+
+        if (try dvui.menuItemLabel(@src(), "File", .{ .submenu = true }, .{ .expand = .none })) |r| {
+            var fw = try dvui.floatingMenu(@src(), dvui.Rect.fromPoint(dvui.Point{ .x = r.x, .y = r.y + r.h }), .{});
+            defer fw.deinit();
+
+            if (try dvui.menuItemLabel(@src(), "Close Menu", .{}, .{}) != null) {
+                dvui.menuGet().?.close();
+            }
+        }
+
+        if (try dvui.menuItemLabel(@src(), "Edit", .{ .submenu = true }, .{ .expand = .none })) |r| {
+            var fw = try dvui.floatingMenu(@src(), dvui.Rect.fromPoint(dvui.Point{ .x = r.x, .y = r.y + r.h }), .{});
+            defer fw.deinit();
+            _ = try dvui.menuItemLabel(@src(), "Dummy", .{}, .{ .expand = .horizontal });
+            _ = try dvui.menuItemLabel(@src(), "Dummy Long", .{}, .{ .expand = .horizontal });
+            _ = try dvui.menuItemLabel(@src(), "Dummy Super Long", .{}, .{ .expand = .horizontal });
+        }
+    }
+
+    var scroll = try dvui.scrollArea(@src(), .{}, .{ .expand = .both, .color_fill = .{ .name = .fill_window } });
+    defer scroll.deinit();
+
+    var tl = try dvui.textLayout(@src(), .{}, .{ .expand = .horizontal, .font_style = .title_4 });
+    const lorem = "This example shows how to use dvui in a web canvas.";
+    try tl.addText(lorem, .{});
+    tl.deinit();
+
+    var tl2 = try dvui.textLayout(@src(), .{}, .{ .expand = .horizontal });
+    try tl2.addText(
+        \\DVUI
+        \\- paints the entire window
+        \\- can show floating windows and dialogs
+        \\- example menu at the top of the window
+        \\- rest of the window is a scroll area
+    , .{});
+    try tl2.addText("\n\n", .{});
+    try tl2.addText("Framerate is variable and adjusts as needed for input events and animations.", .{});
+    try tl2.addText("\n\n", .{});
+    try tl2.addText("Cursor is always being set by dvui.", .{});
+    tl2.deinit();
+
+    if (dvui.Examples.show_demo_window) {
+        if (try dvui.button(@src(), "Hide Demo Window", .{}, .{})) {
+            dvui.Examples.show_demo_window = false;
+        }
+    } else {
+        if (try dvui.button(@src(), "Show Demo Window", .{}, .{})) {
+            dvui.Examples.show_demo_window = true;
+        }
+    }
+
+    // look at demo() for examples of dvui widgets, shows in a floating window
+    try dvui.Examples.demo();
 }
