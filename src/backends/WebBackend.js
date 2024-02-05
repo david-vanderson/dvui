25a426328fbe560b8778d164e947fbded2c81e39
David Vanderson <david.vanderson@gmail.com>
2024-02-05 15:15:23 -0500

    web backend: refresh on browser window resize

--- src/backends/WebBackend.js
+++ src/backends/WebBackend.js
@@ -5,48 +5,48 @@ async function dvui_sleep(ms) {
 
 function dvui(canvasId, wasmFile) {
 
-  const vertexShaderSource = `# version 300 es
+    const vertexShaderSource = `# version 300 es
 
-    precision mediump float;
+        precision mediump float;
 
-    in vec4 aVertexPosition;
-    in vec4 aVertexColor;
-    in vec2 aTextureCoord;
+        in vec4 aVertexPosition;
+        in vec4 aVertexColor;
+        in vec2 aTextureCoord;
 
-    uniform mat4 uMatrix;
+        uniform mat4 uMatrix;
 
-    out vec4 vColor;
-    out vec2 vTextureCoord;
+        out vec4 vColor;
+        out vec2 vTextureCoord;
 
-    void main() {
-      gl_Position = uMatrix * aVertexPosition;
-      vColor = aVertexColor / 255.0;  // normalize u8 colors to 0-1
-      vColor.rgb *= vColor.a;  // convert to premultiplied alpha
-      vTextureCoord = aTextureCoord;
-    }
-  `;
-
-  const fragmentShaderSource = `# version 300 es
-
-    precision mediump float;
-
-    in vec4 vColor;
-    in vec2 vTextureCoord;
-
-    uniform sampler2D uSampler;
-    uniform bool useTex;
-
-    out vec4 fragColor;
-
-    void main() {
-        if (useTex) {
-            fragColor = texture(uSampler, vTextureCoord) * vColor;
+        void main() {
+          gl_Position = uMatrix * aVertexPosition;
+          vColor = aVertexColor / 255.0;  // normalize u8 colors to 0-1
+          vColor.rgb *= vColor.a;  // convert to premultiplied alpha
+          vTextureCoord = aTextureCoord;
         }
-        else {
-            fragColor = vColor;
+    `;
+
+    const fragmentShaderSource = `# version 300 es
+
+        precision mediump float;
+
+        in vec4 vColor;
+        in vec2 vTextureCoord;
+
+        uniform sampler2D uSampler;
+        uniform bool useTex;
+
+        out vec4 fragColor;
+
+        void main() {
+            if (useTex) {
+                fragColor = texture(uSampler, vTextureCoord) * vColor;
+            }
+            else {
+                fragColor = vColor;
+            }
         }
-    }
-  `;
+    `;
 
     let gl;
     let indexBuffer;
@@ -64,21 +64,21 @@ function dvui(canvasId, wasmFile) {
     const utf8encoder = new TextEncoder();
 
     const imports = {
-      env: {
+        env: {
         wasm_panic: (ptr, len) => {
-          let msg = utf8decoder.decode(new Uint8Array(wasmResult.instance.exports.memory.buffer, ptr, len));
-          alert(msg);
-          throw Error(msg);
+            let msg = utf8decoder.decode(new Uint8Array(wasmResult.instance.exports.memory.buffer, ptr, len));
+            alert(msg);
+            throw Error(msg);
         },
         wasm_log_write: (ptr, len) => {
-          log_string += utf8decoder.decode(new Uint8Array(wasmResult.instance.exports.memory.buffer, ptr, len));
+            log_string += utf8decoder.decode(new Uint8Array(wasmResult.instance.exports.memory.buffer, ptr, len));
         },
         wasm_log_flush: () => {
-          console.log(log_string);
-          log_string = '';
+            console.log(log_string);
+            log_string = '';
         },
         wasm_now() {
-          return performance.now();
+            return performance.now();
         },
         wasm_sleep(ms) {
             dvui_sleep(ms);
@@ -100,120 +100,120 @@ function dvui(canvasId, wasmFile) {
             return gl.scissor(x, y, w, h);
         },
         wasm_clear() {
-          gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
-          gl.clear(gl.COLOR_BUFFER_BIT);
+            gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
+            gl.clear(gl.COLOR_BUFFER_BIT);
         },
         wasm_textureCreate(pixels, width, height) {
-          const pixelData = new Uint8Array(wasmResult.instance.exports.memory.buffer, pixels, width * height * 4);
+            const pixelData = new Uint8Array(wasmResult.instance.exports.memory.buffer, pixels, width * height * 4);
 
-          const texture = gl.createTexture();
-          const id = newTextureId;
-          //console.log("creating texture " + id);
-          newTextureId += 1;
-          textures.set(id, texture);
+            const texture = gl.createTexture();
+            const id = newTextureId;
+            //console.log("creating texture " + id);
+            newTextureId += 1;
+            textures.set(id, texture);
           
-          gl.bindTexture(gl.TEXTURE_2D, texture);
+            gl.bindTexture(gl.TEXTURE_2D, texture);
 
             gl.texImage2D(
-              gl.TEXTURE_2D,
-              0,
-              gl.RGBA,
+                gl.TEXTURE_2D,
+                0,
+                gl.RGBA,
                 width,
                 height,
                 0,
-              gl.RGBA,
-              gl.UNSIGNED_BYTE,
-              pixelData,
+                gl.RGBA,
+                gl.UNSIGNED_BYTE,
+                pixelData,
             );
 
-          gl.generateMipmap(gl.TEXTURE_2D);
+            gl.generateMipmap(gl.TEXTURE_2D);
 
-          return id;
+            return id;
         },
         wasm_textureDestroy(id) {
-          //console.log("deleting texture " + id);
-          const texture = textures.get(id);
-          textures.delete(id);
-          
-          gl.deleteTexture(texture);
+            //console.log("deleting texture " + id);
+            const texture = textures.get(id);
+            textures.delete(id);
+
+            gl.deleteTexture(texture);
         },
         wasm_renderGeometry(textureId, index_ptr, index_len, vertex_ptr, vertex_len, sizeof_vertex, offset_pos, offset_col, offset_uv, x, y, w, h) {
-          //console.log("renderGeometry " + textureId + " sizeof " + sizeof_vertex + " pos " + offset_pos + " col " + offset_col + " uv " + offset_uv);
-          
-          const old_scissor = gl.getParameter(gl.SCISSOR_BOX);
-          gl.scissor(x, y, w, h);
+            //console.log("renderGeometry " + textureId + " sizeof " + sizeof_vertex + " pos " + offset_pos + " col " + offset_col + " uv " + offset_uv);
 
-          gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-          const indices = new Uint32Array(wasmResult.instance.exports.memory.buffer, index_ptr, index_len / 4);
-          gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+            const old_scissor = gl.getParameter(gl.SCISSOR_BOX);
+            gl.scissor(x, y, w, h);
 
-          gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-          const vertexes = new Uint8Array(wasmResult.instance.exports.memory.buffer, vertex_ptr, vertex_len);
-          gl.bufferData( gl.ARRAY_BUFFER, vertexes, gl.STATIC_DRAW);
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+            const indices = new Uint32Array(wasmResult.instance.exports.memory.buffer, index_ptr, index_len / 4);
+            gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 
-          let matrix = new Float32Array(16);
-          matrix[0] = 2.0 / gl.canvas.clientWidth;
-          matrix[1] = 0.0;
-          matrix[2] = 0.0;
-          matrix[3] = 0.0;
-          matrix[4] = 0.0;
-          matrix[5] = -2.0 / gl.canvas.clientHeight;
-          matrix[6] = 0.0;
-          matrix[7] = 0.0;
-          matrix[8] = 0.0;
-          matrix[9] = 0.0;
-          matrix[10] = 1.0;
-          matrix[11] = 0.0;
-          matrix[12] = -1.0;
-          matrix[13] = 1.0;
-          matrix[14] = 0.0;
-          matrix[15] = 1.0;
+            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+            const vertexes = new Uint8Array(wasmResult.instance.exports.memory.buffer, vertex_ptr, vertex_len);
+            gl.bufferData( gl.ARRAY_BUFFER, vertexes, gl.STATIC_DRAW);
 
-          // vertex
-          gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-          gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexPosition,
-            2,  // num components
-            gl.FLOAT,
-            false,  // don't normalize
-            sizeof_vertex,  // stride
-            offset_pos,  // offset
-          );
-          gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
+            let matrix = new Float32Array(16);
+            matrix[0] = 2.0 / gl.canvas.clientWidth;
+            matrix[1] = 0.0;
+            matrix[2] = 0.0;
+            matrix[3] = 0.0;
+            matrix[4] = 0.0;
+            matrix[5] = -2.0 / gl.canvas.clientHeight;
+            matrix[6] = 0.0;
+            matrix[7] = 0.0;
+            matrix[8] = 0.0;
+            matrix[9] = 0.0;
+            matrix[10] = 1.0;
+            matrix[11] = 0.0;
+            matrix[12] = -1.0;
+            matrix[13] = 1.0;
+            matrix[14] = 0.0;
+            matrix[15] = 1.0;
 
-          // color
-          gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-          gl.vertexAttribPointer(
-            programInfo.attribLocations.vertexColor,
-            4,  // num components
-            gl.UNSIGNED_BYTE,
-            false,  // don't normalize
-            sizeof_vertex, // stride
-            offset_col,  // offset
-          );
-          gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
+            // vertex
+            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+            gl.vertexAttribPointer(
+                programInfo.attribLocations.vertexPosition,
+                2,  // num components
+                gl.FLOAT,
+                false,  // don't normalize
+                sizeof_vertex,  // stride
+                offset_pos,  // offset
+            );
+            gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
 
-          // texture
-          gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-          gl.vertexAttribPointer(
+            // color
+            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+            gl.vertexAttribPointer(
+                programInfo.attribLocations.vertexColor,
+                4,  // num components
+                gl.UNSIGNED_BYTE,
+                false,  // don't normalize
+                sizeof_vertex, // stride
+                offset_col,  // offset
+            );
+            gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
+
+            // texture
+            gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+            gl.vertexAttribPointer(
             programInfo.attribLocations.textureCoord,
-            2,  // num components
-            gl.FLOAT,
-            false,  // don't normalize
-            sizeof_vertex, // stride
-            offset_uv,  // offset
-          );
-          gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
+                2,  // num components
+                gl.FLOAT,
+                false,  // don't normalize
+                sizeof_vertex, // stride
+                offset_uv,  // offset
+            );
+            gl.enableVertexAttribArray(programInfo.attribLocations.textureCoord);
 
-          // Tell WebGL to use our program when drawing
-          gl.useProgram(shaderProgram);
+            // Tell WebGL to use our program when drawing
+            gl.useProgram(shaderProgram);
 
-          // Set the shader uniforms
-          gl.uniformMatrix4fv(
+            // Set the shader uniforms
+            gl.uniformMatrix4fv(
             programInfo.uniformLocations.matrix,
             false,
             matrix,
-          );
+            );
 
             if (textureId != 0) {
                 gl.activeTexture(gl.TEXTURE0);
@@ -239,55 +239,55 @@ function dvui(canvasId, wasmFile) {
 
         wasmResult = result;
 
-          const canvas = document.querySelector(canvasId);
-          // Initialize the GL context
-          gl = canvas.getContext("webgl2", { alpha: true });
+        const canvas = document.querySelector(canvasId);
+        // Initialize the GL context
+        gl = canvas.getContext("webgl2", { alpha: true });
 
-          // Only continue if WebGL is available and working
-          if (gl === null) {
+        // Only continue if WebGL is available and working
+        if (gl === null) {
             alert("Unable to initialize WebGL. Your browser or machine may not support it.");
             return;
-          }
+        }
 
-          const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-          gl.shaderSource(vertexShader, vertexShaderSource);
-          gl.compileShader(vertexShader);
-          if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+        const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+        gl.shaderSource(vertexShader, vertexShaderSource);
+        gl.compileShader(vertexShader);
+        if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
             alert(`Error compiling vertex shader: ${gl.getShaderInfoLog(shader)}`);
             gl.deleteShader(vertexShader);
             return null;
-          }
+        }
 
-          const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-          gl.shaderSource(fragmentShader, fragmentShaderSource);
-          gl.compileShader(fragmentShader);
-          if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+        const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+        gl.shaderSource(fragmentShader, fragmentShaderSource);
+        gl.compileShader(fragmentShader);
+        if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
             alert(`Error compiling fragment shader: ${gl.getShaderInfoLog(shader)}`);
             gl.deleteShader(fragmentShader);
             return null;
-          }
+        }
 
-          shaderProgram = gl.createProgram();
-          gl.attachShader(shaderProgram, vertexShader);
-          gl.attachShader(shaderProgram, fragmentShader);
-          gl.linkProgram(shaderProgram);
+        shaderProgram = gl.createProgram();
+        gl.attachShader(shaderProgram, vertexShader);
+        gl.attachShader(shaderProgram, fragmentShader);
+        gl.linkProgram(shaderProgram);
 
-          if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
             alert(`Error initializing shader program: ${gl.getProgramInfoLog(shaderProgram)}`);
             return null;
-          }
+        }
 
         programInfo = {
-          attribLocations: {
-            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-            vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
-            textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
-          },
-          uniformLocations: {
-            matrix: gl.getUniformLocation(shaderProgram, "uMatrix"),
-            uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
-            useTex: gl.getUniformLocation(shaderProgram, "useTex"),
-          },
+            attribLocations: {
+                vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+                vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
+                textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
+            },
+            uniformLocations: {
+                matrix: gl.getUniformLocation(shaderProgram, "uMatrix"),
+                uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
+                useTex: gl.getUniformLocation(shaderProgram, "useTex"),
+            },
         };
 
         indexBuffer = gl.createBuffer();
@@ -304,22 +304,22 @@ function dvui(canvasId, wasmFile) {
         wasmResult.instance.exports.app_init();
 
         function render() {
-          renderRequested = false;
+            renderRequested = false;
 
-          // if the canvas changed size, adjust the backing buffer
-          const w = gl.canvas.clientWidth;
-          const h = gl.canvas.clientHeight;
-          gl.canvas.width = Math.round(w * scale);
-          gl.canvas.height = Math.round(h * scale);
-          gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+            // if the canvas changed size, adjust the backing buffer
+            const w = gl.canvas.clientWidth;
+            const h = gl.canvas.clientHeight;
+            gl.canvas.width = Math.round(w * scale);
+            gl.canvas.height = Math.round(h * scale);
+            gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
 
-          let millis_to_wait = wasmResult.instance.exports.app_update();
-          if (millis_to_wait == 0) {
-            requestRender();
-          } else if (millis_to_wait > 0) {
-            renderTimeoutId = setTimeout(function () { renderTimeoutId = 0; requestRender(); }, millis_to_wait);
-          }
-          // otherwise something went wrong, so stop
+            let millis_to_wait = wasmResult.instance.exports.app_update();
+            if (millis_to_wait == 0) {
+                requestRender();
+            } else if (millis_to_wait > 0) {
+                renderTimeoutId = setTimeout(function () { renderTimeoutId = 0; requestRender(); }, millis_to_wait);
+            }
+            // otherwise something went wrong, so stop
         }
 
         function requestRender() {
@@ -342,24 +342,27 @@ function dvui(canvasId, wasmFile) {
         canvas.addEventListener("contextmenu", (ev) => {
             ev.preventDefault();
         });
+        window.addEventListener("resize", (ev) => {
+            requestRender();
+        });
         canvas.addEventListener("mousemove", (ev) => {
-          var rect = canvas.getBoundingClientRect();
-          var x = (ev.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
-          var y = (ev.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
-          wasmResult.instance.exports.add_event(1, 0, 0, x, y);
-          requestRender();
+            var rect = canvas.getBoundingClientRect();
+            var x = (ev.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
+            var y = (ev.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
+            wasmResult.instance.exports.add_event(1, 0, 0, x, y);
+            requestRender();
         });
         canvas.addEventListener("mousedown", (ev) => {
-          wasmResult.instance.exports.add_event(2, ev.button, 0, 0, 0);
-          requestRender();
+            wasmResult.instance.exports.add_event(2, ev.button, 0, 0, 0);
+            requestRender();
         });
         canvas.addEventListener("mouseup", (ev) => {
-          wasmResult.instance.exports.add_event(3, ev.button, 0, 0, 0);
-          requestRender();
+            wasmResult.instance.exports.add_event(3, ev.button, 0, 0, 0);
+            requestRender();
         });
         canvas.addEventListener("wheel", (ev) => {
-          wasmResult.instance.exports.add_event(4, 0, 0, ev.deltaY, 0);
-          requestRender();
+            wasmResult.instance.exports.add_event(4, 0, 0, ev.deltaY, 0);
+            requestRender();
         });
 
         // start the first update
