7000cf0a329252de2bef4f81aa717c323532bf27
David Vanderson <david.vanderson@gmail.com>
2024-04-11 21:45:26 -0400

    theme adwaita: use HSLuv for specifying colors

--- src/themes/Adwaita.zig
+++ src/themes/Adwaita.zig
@@ -7,7 +7,21 @@ const Options = dvui.Options;
 const bitstream_vera = dvui.bitstream_vera;
 
 const accent = Color{ .r = 0x35, .g = 0x84, .b = 0xe4 };
+const accent_hsl = Color.HSLuv.fromColor(accent);
 const err = Color{ .r = 0xe0, .g = 0x1b, .b = 0x24 };
+const err_hsl = Color.HSLuv.fromColor(err);
+
+// need to have these as separate variables because inlining them below trips
+// zig's comptime eval quota
+const light_accent_accent = accent_hsl.lighten(-16).color();
+const light_accent_fill = accent_hsl.color();
+const light_accent_fill_hover = accent_hsl.lighten(-11).color();
+const light_accent_border = accent_hsl.lighten(-22).color();
+
+const light_err_accent = err_hsl.lighten(-15).color();
+const light_err_fill = err_hsl.color();
+const light_err_fill_hover = err_hsl.lighten(-10).color();
+const light_err_border = err_hsl.lighten(-20).color();
 
 pub var light = Theme{
     .name = "Adwaita",
@@ -23,40 +37,51 @@ pub var light = Theme{
     .font_title_3 = .{ .size = 17, .name = "VeraBd", .ttf_bytes = bitstream_vera.VeraBd },
     .font_title_4 = .{ .size = 15, .name = "VeraBd", .ttf_bytes = bitstream_vera.VeraBd },
 
-    .color_accent = accent,
-    .color_err = err,
+    .color_accent = accent_hsl.color(),
+    .color_err = err_hsl.color(),
     .color_text = Color.black,
     .color_text_press = Color.black,
     .color_fill = Color.white,
     .color_fill_window = .{ .r = 0xf0, .g = 0xf0, .b = 0xf0 },
     .color_fill_control = .{ .r = 0xe0, .g = 0xe0, .b = 0xe0 },
-    .color_fill_hover = Color.lerp(Color.white, 0.2, Color.black),
-    .color_fill_press = Color.lerp(Color.white, 0.3, Color.black),
-    .color_border = Color.lerp(Color.white, 0.4, Color.black),
+    .color_fill_hover = (Color.HSLuv{ .s = 0, .l = 82 }).color(),
+    .color_fill_press = (Color.HSLuv{ .s = 0, .l = 72 }).color(),
+    .color_border = (Color.HSLuv{ .s = 0, .l = 63 }).color(),
 
     .style_accent = Options{
-        .color_accent = .{ .color = accent.darken(0.3) },
+        .color_accent = .{ .color = light_accent_accent },
         .color_text = .{ .color = Color.white },
         .color_text_press = .{ .color = Color.white },
-        .color_fill = .{ .color = accent },
-        .color_fill_hover = .{ .color = Color.lerp(accent, 0.2, Color.black) },
-        .color_fill_press = .{ .color = Color.lerp(accent, 0.3, Color.black) },
-        .color_border = .{ .color = Color.lerp(accent, 0.4, Color.black) },
+        .color_fill = .{ .color = light_accent_fill },
+        .color_fill_hover = .{ .color = light_accent_fill_hover },
+        .color_fill_press = .{ .color = light_accent_accent },
+        .color_border = .{ .color = light_accent_border },
     },
 
     .style_err = Options{
-        .color_accent = .{ .color = err.darken(0.3) },
+        .color_accent = .{ .color = light_err_accent },
         .color_text = .{ .color = Color.white },
         .color_text_press = .{ .color = Color.white },
-        .color_fill = .{ .color = err },
-        .color_fill_hover = .{ .color = Color.lerp(err, 0.2, Color.black) },
-        .color_fill_press = .{ .color = Color.lerp(err, 0.3, Color.black) },
-        .color_border = .{ .color = Color.lerp(err, 0.4, Color.black) },
+        .color_fill = .{ .color = light_err_fill },
+        .color_fill_hover = .{ .color = light_err_fill_hover },
+        .color_fill_press = .{ .color = light_err_accent },
+        .color_border = .{ .color = light_err_border },
     },
 };
 
 const dark_fill = Color{ .r = 0x1e, .g = 0x1e, .b = 0x1e };
+const dark_fill_hsl = Color.HSLuv.fromColor(dark_fill);
 const dark_err = Color{ .r = 0xc0, .g = 0x1c, .b = 0x28 };
+const dark_err_hsl = Color.HSLuv.fromColor(dark_err);
+
+const dark_accent_accent = accent_hsl.lighten(12).color();
+const dark_accent_fill_hover = accent_hsl.lighten(9).color();
+const dark_accent_border = accent_hsl.lighten(17).color();
+
+const dark_err_accent = dark_err_hsl.lighten(14).color();
+const dark_err_fill_hover = err_hsl.lighten(9).color();
+const dark_err_fill_press = err_hsl.lighten(16).color();
+const dark_err_border = err_hsl.lighten(20).color();
 
 pub var dark = Theme{
     .name = "Adwaita Dark",
@@ -72,34 +97,34 @@ pub var dark = Theme{
     .font_title_3 = .{ .size = 17, .name = "VeraBd", .ttf_bytes = bitstream_vera.VeraBd },
     .font_title_4 = .{ .size = 15, .name = "VeraBd", .ttf_bytes = bitstream_vera.VeraBd },
 
-    .color_accent = accent,
+    .color_accent = accent_hsl.color(),
     .color_err = dark_err,
     .color_text = Color.white,
     .color_text_press = Color.white,
     .color_fill = dark_fill,
     .color_fill_window = .{ .r = 0x2b, .g = 0x2b, .b = 0x2b },
     .color_fill_control = .{ .r = 0x40, .g = 0x40, .b = 0x40 },
-    .color_fill_hover = Color.lerp(dark_fill, 0.2, Color.white),
-    .color_fill_press = Color.lerp(dark_fill, 0.3, Color.white),
-    .color_border = Color.lerp(dark_fill, 0.4, Color.white),
+    .color_fill_hover = dark_fill_hsl.lighten(21).color(),
+    .color_fill_press = dark_fill_hsl.lighten(30).color(),
+    .color_border = dark_fill_hsl.lighten(39).color(),
 
     .style_accent = Options{
-        .color_accent = .{ .color = accent.lighten(0.3) },
+        .color_accent = .{ .color = dark_accent_accent },
         .color_text = .{ .color = Color.white },
         .color_text_press = .{ .color = Color.white },
         .color_fill = .{ .color = accent },
-        .color_fill_hover = .{ .color = Color.lerp(accent, 0.2, Color.white) },
-        .color_fill_press = .{ .color = Color.lerp(accent, 0.3, Color.white) },
-        .color_border = .{ .color = Color.lerp(accent, 0.4, Color.white) },
+        .color_fill_hover = .{ .color = dark_accent_fill_hover },
+        .color_fill_press = .{ .color = dark_accent_accent },
+        .color_border = .{ .color = dark_accent_border },
     },
 
     .style_err = Options{
-        .color_accent = .{ .color = dark_err.lighten(0.3) },
+        .color_accent = .{ .color = dark_err_accent },
         .color_text = .{ .color = Color.white },
         .color_text_press = .{ .color = Color.white },
         .color_fill = .{ .color = dark_err },
-        .color_fill_hover = .{ .color = Color.lerp(err, 0.2, Color.white) },
-        .color_fill_press = .{ .color = Color.lerp(err, 0.3, Color.white) },
-        .color_border = .{ .color = Color.lerp(err, 0.4, Color.white) },
+        .color_fill_hover = .{ .color = dark_err_fill_hover },
+        .color_fill_press = .{ .color = dark_err_fill_press },
+        .color_border = .{ .color = dark_err_border },
     },
 };
