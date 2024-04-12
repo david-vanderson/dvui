f9d859266ab46ba5d6d2520d5c901d4fb202b484
David Vanderson <david.vanderson@gmail.com>
2024-04-11 21:59:07 -0400

    Color: remove now-unused old darken/lighten/lerp functions
    Instead code should use the Color.HSLuv stuff to modify colors.

--- src/Color.zig
+++ src/Color.zig
@@ -52,33 +52,6 @@ pub fn transparent(x: Color, y: f32) Color {
     };
 }
 
-pub fn darken(x: Color, y: f32) Color {
-    return Color{
-        .r = @as(u8, @intFromFloat(@max(@as(f32, @floatFromInt(x.r)) * (1 - y), 0))),
-        .g = @as(u8, @intFromFloat(@max(@as(f32, @floatFromInt(x.g)) * (1 - y), 0))),
-        .b = @as(u8, @intFromFloat(@max(@as(f32, @floatFromInt(x.b)) * (1 - y), 0))),
-        .a = x.a,
-    };
-}
-
-pub fn lighten(x: Color, y: f32) Color {
-    return Color{
-        .r = @as(u8, @intFromFloat(@min(@as(f32, @floatFromInt(x.r)) * (1 + y), 255))),
-        .g = @as(u8, @intFromFloat(@min(@as(f32, @floatFromInt(x.g)) * (1 + y), 255))),
-        .b = @as(u8, @intFromFloat(@min(@as(f32, @floatFromInt(x.b)) * (1 + y), 255))),
-        .a = x.a,
-    };
-}
-
-pub fn lerp(x: Color, y: f32, z: Color) Color {
-    return Color{
-        .r = @as(u8, @intFromFloat(@as(f32, @floatFromInt(x.r)) * (1 - y) + @as(f32, @floatFromInt(z.r)) * y)),
-        .g = @as(u8, @intFromFloat(@as(f32, @floatFromInt(x.g)) * (1 - y) + @as(f32, @floatFromInt(z.g)) * y)),
-        .b = @as(u8, @intFromFloat(@as(f32, @floatFromInt(x.b)) * (1 - y) + @as(f32, @floatFromInt(z.b)) * y)),
-        .a = @as(u8, @intFromFloat(@as(f32, @floatFromInt(x.a)) * (1 - y) + @as(f32, @floatFromInt(z.a)) * y)),
-    };
-}
-
 pub fn format(self: *const Color, comptime _: []const u8, _: std.fmt.FormatOptions, writer: anytype) !void {
     try std.fmt.format(writer, "Color{{ {x} {x} {x} {x} }}", .{ self.r, self.g, self.b, self.a });
 }
