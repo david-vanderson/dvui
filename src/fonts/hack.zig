0632b598acd7e6c8a81df86b2d6e7ab3154ba11b
Robert Burnett <dr.slate2@gmail.com>
2024-07-29 16:03:19 -0500

    added file

---
+++ src/fonts/hack.zig
@@ -0,0 +1,4 @@
+pub const Hack = @embedFile("hack/Hack-Regular.ttf");
+pub const HackBd = @embedFile("hack/Hack-Bold.ttf");
+pub const HackIt = @embedFile("hack/Hack-Italic.ttf");
+pub const HackBdIt = @embedFile("hack/Hack-BoldItalic.ttf");
