5483d37398d8f9a576633fb26e7bd017ad62da9b
Robert Burnett <dr.slate2@gmail.com>
2024-07-29 16:52:14 -0500

    Replaced old font, reworked jungle style

---
+++ src/fonts/pixelify-sans.zig
@@ -0,0 +1,4 @@
+pub const PixelifySans = @embedFile("Pixelify_Sans/static/PixelifySans-Regular.ttf");
+pub const PixelifySansBd = @embedFile("Pixelify_Sans/static/PixelifySans-Bold.ttf");
+pub const PixelifySansMe = @embedFile("Pixelify_Sans/static/PixelifySans-Medium.ttf");
+pub const PixelifySansSeBd = @embedFile("Pixelify_Sans/static/PixelifySans-SemiBold.ttf");
