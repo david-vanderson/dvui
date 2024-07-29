6f6a917c24a7686356c42973d7d1338d3536c053
Robert Burnett <dr.slate2@gmail.com>
2024-07-29 13:57:42 -0500

    added dracula theme

---
+++ src/themes/Dracula.zig
@@ -0,0 +1,128 @@
+const dvui = @import("../dvui.zig");
+
+const Color = dvui.Color;
+const Font = dvui.Font;
+const Theme = dvui.Theme;
+const Options = dvui.Options;
+const hack = dvui.hack;
+
+//Colors Derived from Dracula theme for Kitty
+const border = Color{ .r = 0x62, .g = 0x72, .b = 0xa4, .a = 0xff }; // inactive_border_color
+const fill = Color{ .r = 0x44, .g = 0x47, .b = 0x5a, .a = 0xff }; // selection_background
+const text = Color{ .r = 0xf8, .g = 0xf8, .b = 0xf2, .a = 0xff }; // foreground
+const border_hover = Color{ .r = 0xf8, .g = 0xf8, .b = 0xf2, .a = 0xff }; // active_border_color
+const fill_hover = Color{ .r = 0x62, .g = 0x72, .b = 0xa4, .a = 0xff }; // inactive_tab_background
+const text_hover = Color{ .r = 0xff, .g = 0xff, .b = 0xff, .a = 0xff }; // selection_foreground
+const border_pressed = Color{ .r = 0x28, .g = 0x2a, .b = 0x36, .a = 0xff }; // active_tab_foreground
+const accent = Color{ .r = 0xff, .g = 0x79, .b = 0xc6, .a = 0xff };
+const fill_pressed = accent;
+const text_accent = Color{ .r = 0x8b, .g = 0xe9, .b = 0xfd, .a = 0xff }; // url_color
+const border_err = Color{ .r = 0xff, .g = 0x55, .b = 0x55, .a = 0xff }; // color1
+const err = Color{ .r = 0xf8, .g = 0xf8, .b = 0xf2, .a = 0xff }; // color7
+const background = Color{ .r = 0x28, .g = 0x2a, .b = 0x36, .a = 0xff }; // background
+
+const size = 13;
+
+pub var dracula = Theme{
+    .name = "Dracula",
+    .dark = true,
+
+    .font_body = .{ .size = 13, .name = "hack", .ttf_bytes = hack },
+    .font_heading = .{ .size = 13, .name = "hack", .ttf_bytes = hack },
+    .font_caption = .{ .size = 10, .name = "hack", .ttf_bytes = hack },
+    .font_caption_heading = .{ .size = 10, .name = "hack", .ttf_bytes = hack },
+    .font_title = .{ .size = 28, .name = "hack", .ttf_bytes = hack },
+    .font_title_1 = .{ .size = 23, .name = "hack", .ttf_bytes = hack },
+    .font_title_2 = .{ .size = 20, .name = "hack", .ttf_bytes = hack },
+    .font_title_3 = .{ .size = 17, .name = "hack", .ttf_bytes = hack },
+    .font_title_4 = .{ .size = 15, .name = "hack", .ttf_bytes = hack },
+
+    .color_accent = accent,
+    .color_err = err,
+    .color_text = text,
+    .color_text_press = text_accent,
+    .color_fill = fill,
+    .color_fill_window = background,
+    .color_fill_control = fill,
+    .color_fill_hover = fill_hover,
+    .color_fill_press = accent,
+    .color_border = border,
+
+    .style_accent = Options{
+        .color_accent = .{ .color = accent },
+        .color_text = .{ .color = text_accent },
+        .color_text_press = .{ .color = Color.white },
+        .color_fill = .{ .color = fill },
+        .color_fill_hover = .{ .color = fill_hover },
+        .color_fill_press = .{ .color = fill_pressed },
+        .color_border = .{ .color = border_pressed },
+    },
+
+    .style_err = Options{
+        .color_accent = .{ .color = accent },
+        .color_text = .{ .color = Color.white },
+        .color_text_press = .{ .color = Color.white },
+        .color_fill = .{ .color = err },
+        .color_fill_hover = .{ .color = fill_hover },
+        .color_fill_press = .{ .color = fill_pressed },
+        .color_border = .{ .color = border_pressed },
+    },
+};
+
+//==RAW COLORS==
+//
+//Color{ .r = 0xf8, .g = 0xf8, .b = 0xf2, .a = 0xff },    // foreground
+//Color{ .r = 0x28, .g = 0x2a, .b = 0x36, .a = 0xff },    // background
+//Color{ .r = 0xff, .g = 0xff, .b = 0xff, .a = 0xff },    // selection_foreground
+//Color{ .r = 0x44, .g = 0x47, .b = 0x5a, .a = 0xff },    // selection_background
+//Color{ .r = 0x8b, .g = 0xe9, .b = 0xfd, .a = 0xff },    // url_color
+//
+//// black
+//Color{ .r = 0x21, .g = 0x22, .b = 0x2c, .a = 0xff },    // color0
+//Color{ .r = 0x62, .g = 0x72, .b = 0xa4, .a = 0xff },    // color8
+//
+//// red
+//Color{ .r = 0xff, .g = 0x55, .b = 0x55, .a = 0xff },    // color1
+//Color{ .r = 0xff, .g = 0x6e, .b = 0x6e, .a = 0xff },    // color9
+//
+//// green
+//Color{ .r = 0x50, .g = 0xfa, .b = 0x7b, .a = 0xff },    // color2
+//Color{ .r = 0x69, .g = 0xff, .b = 0x94, .a = 0xff },    // color10
+//
+//// yellow
+//Color{ .r = 0xf1, .g = 0xfa, .b = 0x8c, .a = 0xff },    // color3
+//Color{ .r = 0xff, .g = 0xff, .b = 0xa5, .a = 0xff },    // color11
+//
+//// blue
+//Color{ .r = 0xbd, .g = 0x93, .b = 0xf9, .a = 0xff },    // color4
+//Color{ .r = 0xd6, .g = 0xac, .b = 0xff, .a = 0xff },    // color12
+//
+//// magenta
+//Color{ .r = 0xff, .g = 0x79, .b = 0xc6, .a = 0xff },    // color5
+//Color{ .r = 0xff, .g = 0x92, .b = 0xdf, .a = 0xff },    // color13
+//
+//// cyan
+//Color{ .r = 0x8b, .g = 0xe9, .b = 0xfd, .a = 0xff },    // color6
+//Color{ .r = 0xa4, .g = 0xff, .b = 0xff, .a = 0xff },    // color14
+//
+//// white
+//Color{ .r = 0xf8, .g = 0xf8, .b = 0xf2, .a = 0xff },    // color7
+//Color{ .r = 0xff, .g = 0xff, .b = 0xff, .a = 0xff },    // color15
+//
+//// Cursor colors
+//Color{ .r = 0xf8, .g = 0xf8, .b = 0xf2, .a = 0xff },    // cursor
+//Color{ .r = 0x28, .g = 0x2a, .b = 0x36, .a = 0xff },    // cursor_text_color (same as background)
+//
+//// Tab bar colors
+//Color{ .r = 0x28, .g = 0x2a, .b = 0x36, .a = 0xff },    // active_tab_foreground
+//Color{ .r = 0xf8, .g = 0xf8, .b = 0xf2, .a = 0xff },    // active_tab_background
+//Color{ .r = 0x28, .g = 0x2a, .b = 0x36, .a = 0xff },    // inactive_tab_foreground
+//Color{ .r = 0x62, .g = 0x72, .b = 0xa4, .a = 0xff },    // inactive_tab_background
+//
+//// Marks
+//Color{ .r = 0x28, .g = 0x2a, .b = 0x36, .a = 0xff },    // mark1_foreground
+//Color{ .r = 0xff, .g = 0x55, .b = 0x55, .a = 0xff },    // mark1_background
+//
+//// Splits/Windows
+//Color{ .r = 0xf8, .g = 0xf8, .b = 0xf2, .a = 0xff },    // active_border_color
+//Color{ .r = 0x62, .g = 0x72, .b = 0xa4, .a = 0xff },    // inactive_border_color
--- src/themes/Jungle.zig
+++ src/themes/Jungle.zig
@@ -4,78 +4,44 @@ const Color = dvui.Color;
 const Font = dvui.Font;
 const Theme = dvui.Theme;
 const Options = dvui.Options;
-const bitstream_vera = dvui.bitstream_vera;
+const pixel_intv = dvui.pixel_intv;
 
-//const accent = Color{ .r = 0x35, .g = 0x84, .b = 0xe4 };
-const accent_hsl = Color.HSLuv.fromColor(accent);
-const err = Color{ .r = 0xe0, .g = 0x1b, .b = 0x24 };
-const err_hsl = Color.HSLuv.fromColor(err);
+//638465
 
-const dark_fill = Color{ .r = 0x1e, .g = 0x1e, .b = 0x1e };
-const dark_fill_hsl = Color.HSLuv.fromColor(dark_fill);
-const dark_err = Color{ .r = 0xc0, .g = 0x1c, .b = 0x28 };
-const dark_err_hsl = Color.HSLuv.fromColor(dark_err);
-
-const dark_accent_accent = accent_hsl.lighten(12).color();
-const dark_accent_fill_hover = accent_hsl.lighten(9).color();
-const dark_accent_border = accent_hsl.lighten(17).color();
-
-const dark_err_accent = dark_err_hsl.lighten(14).color();
-const dark_err_fill_hover = err_hsl.lighten(9).color();
-const dark_err_fill_press = err_hsl.lighten(16).color();
-const dark_err_border = err_hsl.lighten(20).color();
-
+//Colors Derived from raygui jungle theme
 const border = Color{ .r = 0x60, .g = 0x82, .b = 0x7d, .a = 0xff }; // DEFAULT_BORDER_COLOR_NORMAL
 const fill = Color{ .r = 0x2c, .g = 0x33, .b = 0x34, .a = 0xff }; // DEFAULT_BASE_COLOR_NORMAL
-//Color{ .r = 0x82, .g = 0xa2, .b = 0x9f, .a = 0xff },    // DEFAULT_TEXT_COLOR_NORMAL
-const border_hover = Color{ .r = 0x5f, .g = 0x9a, .b = 0xa8, .a = 0xff }; // DEFAULT_BORDER_COLOR_FOCUSED
+const text = Color{ .r = 0x82, .g = 0xa2, .b = 0x9f, .a = 0xff }; // DEFAULT_TEXT_COLOR_NORMAL
+const border_hover = border_pressed; //Color{ .r = 0x5f, .g = 0x9a, .b = 0xa8, .a = 0xff }; // DEFAULT_BORDER_COLOR_FOCUSED
 const fill_hover = Color{ .r = 0x33, .g = 0x4e, .b = 0x57, .a = 0xff }; // DEFAULT_BASE_COLOR_FOCUSED
-//Color{ .r = 0x6a, .g = 0xa9, .b = 0xb8, .a = 0xff },    // DEFAULT_TEXT_COLOR_FOCUSED
-//Color{ .r = 0xa9, .g = 0xcb, .b = 0x8d, .a = 0xff },    // DEFAULT_BORDER_COLOR_PRESSED
+const text_hover = Color{ .r = 0x6a, .g = 0xa9, .b = 0xb8, .a = 0xff }; // DEFAULT_TEXT_COLOR_FOCUSED
+const border_pressed = Color{ .r = 0xa9, .g = 0xcb, .b = 0x8d, .a = 0xff }; // DEFAULT_BORDER_COLOR_PRESSED
 const accent = Color{ .r = 0x3b, .g = 0x63, .b = 0x57, .a = 0xff }; // DEFAULT_BASE_COLOR_PRESSED
+const fill_pressed = accent;
 const text_accent = Color{ .r = 0x97, .g = 0xaf, .b = 0x81, .a = 0xff }; // DEFAULT_TEXT_COLOR_PRESSED
-//Color{ .r = 0x5b, .g = 0x64, .b = 0x62, .a = 0xff },    // DEFAULT_BORDER_COLOR_DISABLED
-//Color{ .r = 0x2c, .g = 0x33, .b = 0x34, .a = 0xff },    // DEFAULT_BASE_COLOR_DISABLED
-//Color{ .r = 0x66, .g = 0x6b, .b = 0x69, .a = 0xff },    // DEFAULT_TEXT_COLOR_DISABLED
-//Color{ .r = 0x63, .g = 0x84, .b = 0x65, .a = 0xff },    // DEFAULT_LINE_COLOR
+const border_err = Color{ .r = 0x5b, .g = 0x64, .b = 0x62, .a = 0xff }; // DEFAULT_BORDER_COLOR_DISABLED
+const err = Color{ .r = 0x66, .g = 0x6b, .b = 0x69, .a = 0xff }; // DEFAULT_TEXT_COLOR_DISABLED
 const background = Color{ .r = 0x2b, .g = 0x3a, .b = 0x3a, .a = 0xff }; // DEFAULT_BACKGROUND_COLOR
-//Color{ .r = 0x00, .g = 0x00, .b = 0x00, .a = 0x12 },    // DEFAULT_TEXT_LINE_SPACING
+
+const size = 13;
 
 pub var jungle = Theme{
     .name = "Jungle",
     .dark = true,
 
-    .font_body = .{ .size = 13, .name = "Vera", .ttf_bytes = bitstream_vera.Vera },
-    .font_heading = .{ .size = 13, .name = "VeraBd", .ttf_bytes = bitstream_vera.VeraBd },
-    .font_caption = .{ .size = 10, .name = "Vera", .ttf_bytes = bitstream_vera.Vera },
-    .font_caption_heading = .{ .size = 10, .name = "VeraBd", .ttf_bytes = bitstream_vera.VeraBd },
-    .font_title = .{ .size = 28, .name = "Vera", .ttf_bytes = bitstream_vera.Vera },
-    .font_title_1 = .{ .size = 23, .name = "VeraBd", .ttf_bytes = bitstream_vera.VeraBd },
-    .font_title_2 = .{ .size = 20, .name = "VeraBd", .ttf_bytes = bitstream_vera.VeraBd },
-    .font_title_3 = .{ .size = 17, .name = "VeraBd", .ttf_bytes = bitstream_vera.VeraBd },
-    .font_title_4 = .{ .size = 15, .name = "VeraBd", .ttf_bytes = bitstream_vera.VeraBd },
-
-    //Color{ .r = 0x60, .g = 0x82, .b = 0x7d, .a = 0xff },    // DEFAULT_BORDER_COLOR_NORMAL
-    //Color{ .r = 0x2c, .g = 0x33, .b = 0x34, .a = 0xff },    // DEFAULT_BASE_COLOR_NORMAL
-    //Color{ .r = 0x82, .g = 0xa2, .b = 0x9f, .a = 0xff },    // DEFAULT_TEXT_COLOR_NORMAL
-    //Color{ .r = 0x5f, .g = 0x9a, .b = 0xa8, .a = 0xff },    // DEFAULT_BORDER_COLOR_FOCUSED
-    //Color{ .r = 0x33, .g = 0x4e, .b = 0x57, .a = 0xff },    // DEFAULT_BASE_COLOR_FOCUSED
-    //Color{ .r = 0x6a, .g = 0xa9, .b = 0xb8, .a = 0xff },    // DEFAULT_TEXT_COLOR_FOCUSED
-    //Color{ .r = 0xa9, .g = 0xcb, .b = 0x8d, .a = 0xff },    // DEFAULT_BORDER_COLOR_PRESSED
-    //Color{ .r = 0x3b, .g = 0x63, .b = 0x57, .a = 0xff },    // DEFAULT_BASE_COLOR_PRESSED
-    //Color{ .r = 0x97, .g = 0xaf, .b = 0x81, .a = 0xff },    // DEFAULT_TEXT_COLOR_PRESSED
-    //Color{ .r = 0x5b, .g = 0x64, .b = 0x62, .a = 0xff },    // DEFAULT_BORDER_COLOR_DISABLED
-    //Color{ .r = 0x2c, .g = 0x33, .b = 0x34, .a = 0xff },    // DEFAULT_BASE_COLOR_DISABLED
-    //Color{ .r = 0x66, .g = 0x6b, .b = 0x69, .a = 0xff },    // DEFAULT_TEXT_COLOR_DISABLED
-    //Color{ .r = 0x00, .g = 0x00, .b = 0x00, .a = 0x0c },    // DEFAULT_TEXT_SIZE
-    //Color{ .r = 0x00, .g = 0x00, .b = 0x00, .a = 0x00 },    // DEFAULT_TEXT_SPACING
-    //Color{ .r = 0x63, .g = 0x84, .b = 0x65, .a = 0xff },    // DEFAULT_LINE_COLOR
-    //Color{ .r = 0x2b, .g = 0x3a, .b = 0x3a, .a = 0xff },    // DEFAULT_BACKGROUND_COLOR
-    //Color{ .r = 0x00, .g = 0x00, .b = 0x00, .a = 0x12 },    // DEFAULT_TEXT_LINE_SPACING
+    .font_body = .{ .size = size, .name = "pixel_intv", .ttf_bytes = pixel_intv },
+    .font_heading = .{ .size = size, .name = "pixel_intv", .ttf_bytes = pixel_intv },
+    .font_caption = .{ .size = size, .name = "pixel_intv", .ttf_bytes = pixel_intv },
+    .font_caption_heading = .{ .size = size, .name = "pixel_intv", .ttf_bytes = pixel_intv },
+    .font_title = .{ .size = size * 2, .name = "pixel_intv", .ttf_bytes = pixel_intv },
+    .font_title_1 = .{ .size = size * 1.8, .name = "pixel_intv", .ttf_bytes = pixel_intv },
+    .font_title_2 = .{ .size = size * 1.6, .name = "pixel_intv", .ttf_bytes = pixel_intv },
+    .font_title_3 = .{ .size = size * 1.4, .name = "pixel_intv", .ttf_bytes = pixel_intv },
+    .font_title_4 = .{ .size = size * 1.2, .name = "pixel_intv", .ttf_bytes = pixel_intv },
 
     .color_accent = accent,
-    .color_err = dark_err,
-    .color_text = Color{ .r = 0x82, .g = 0xa2, .b = 0x9f, .a = 0xff }, // DEFAULT_TEXT_COLOR_NORMAL
+    .color_err = err,
+    .color_text = text,
     .color_text_press = text_accent,
     .color_fill = fill,
     .color_fill_window = background,
@@ -85,22 +51,22 @@ pub var jungle = Theme{
     .color_border = border,
 
     .style_accent = Options{
-        .color_accent = .{ .color = Color{ .r = 0x60, .g = 0x82, .b = 0x7d, .a = 0xff } }, // DEFAULT_BORDER_COLOR_NORMAL
+        .color_accent = .{ .color = accent },
         .color_text = .{ .color = text_accent },
         .color_text_press = .{ .color = Color.white },
-        .color_fill = .{ .color = Color{ .r = 0x60, .g = 0x82, .b = 0x7d, .a = 0xff } }, // DEFAULT_BORDER_COLOR_NORMAL
-        .color_fill_hover = .{ .color = dark_accent_fill_hover },
-        .color_fill_press = .{ .color = dark_accent_accent },
-        .color_border = .{ .color = dark_accent_border },
+        .color_fill = .{ .color = fill },
+        .color_fill_hover = .{ .color = fill_hover },
+        .color_fill_press = .{ .color = fill_pressed },
+        .color_border = .{ .color = border_pressed },
     },
 
     .style_err = Options{
-        .color_accent = .{ .color = Color{ .r = 0x60, .g = 0x82, .b = 0x7d, .a = 0xff } }, // DEFAULT_BORDER_COLOR_NORMAL
+        .color_accent = .{ .color = accent },
         .color_text = .{ .color = Color.white },
         .color_text_press = .{ .color = Color.white },
-        .color_fill = .{ .color = dark_err },
-        .color_fill_hover = .{ .color = dark_err_fill_hover },
-        .color_fill_press = .{ .color = dark_err_fill_press },
-        .color_border = .{ .color = dark_err_border },
+        .color_fill = .{ .color = err },
+        .color_fill_hover = .{ .color = fill_hover },
+        .color_fill_press = .{ .color = fill_pressed },
+        .color_border = .{ .color = border_pressed },
     },
 };
