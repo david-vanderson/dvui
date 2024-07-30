e1093623353503c906330b4c9c86bbc961187010
Robert Burnett <dr.slate2@gmail.com>
2024-07-30 15:53:15 -0500

    Simplified new font addition code

--- src/fonts/aleo.zig
+++
@@ -1,2 +0,0 @@
-pub const Aleo = @embedFile("Aleo/static/Aleo-Regular.ttf");
-pub const AleoBd = @embedFile("Aleo/static/Aleo-Bold.ttf");
--- src/fonts/hack.zig
+++
@@ -1,4 +0,0 @@
-pub const Hack = @embedFile("hack/Hack-Regular.ttf");
-pub const HackBd = @embedFile("hack/Hack-Bold.ttf");
-pub const HackIt = @embedFile("hack/Hack-Italic.ttf");
-pub const HackBdIt = @embedFile("hack/Hack-BoldItalic.ttf");
