d41357d2baccdcdb0acc124cb0b964fac879e3d4
David Vanderson <david.vanderson@gmail.com>
2024-07-31 22:32:22 -0400

    raylib: integrated code from @VisenDev
    Some progress, but textures are still not working.  Much of this is
    still untested.

--- src/backends/RaylibBackend.zig
+++ src/backends/RaylibBackend.zig
@@ -10,6 +10,49 @@ pub const c = @cImport({
 
 const RaylibBackend = @This();
 
+shader: c.Shader = undefined,
+arena: std.mem.Allocator = undefined,
+
+const vertexSource =
+    \\#version 330
+    \\in vec3 vertexPosition;
+    \\in vec2 vertexTexCoord;
+    \\in vec4 vertexColor;
+    \\out vec2 fragTexCoord;
+    \\out vec4 fragColor;
+    \\uniform mat4 mvp;
+    \\void main()
+    \\{
+    \\    fragTexCoord = vertexTexCoord;
+    \\    fragColor = vertexColor / 255.0;
+    //\\    fragColor.rgb *= fragColor.a;
+    \\    gl_Position = mvp*vec4(vertexPosition, 1.0);
+    \\}
+;
+
+const fragSource =
+    \\#version 330
+    \\in vec2 fragTexCoord;
+    \\in vec4 fragColor;
+    \\out vec4 finalColor;
+    \\uniform sampler2D texture0;
+    \\uniform bool useTex;
+    //"uniform vec4 colDiffuse;           \n"
+    //"void main()                        \n"
+    //"{                                  \n"
+    //"    vec4 texelColor = texture(texture0, fragTexCoord);   \n"
+    //"    finalColor = texelColor*colDiffuse*fragColor;        \n"
+    //"}                                  \n";
+    \\void main()
+    \\{
+    \\    if (useTex)
+    \\        finalColor = texture(texture0, fragTexCoord) * fragColor;
+    \\    else
+    \\        finalColor = fragColor;
+    //\\        finalColor = fragColor;
+    \\}
+;
+
 pub const InitOptions = struct {
     /// The allocator used for temporary allocations used during init()
     allocator: std.mem.Allocator,
@@ -29,8 +72,22 @@ pub const InitOptions = struct {
 
 pub fn init(options: InitOptions) !RaylibBackend {
     // TODO: implement all InitOptions
+    c.SetConfigFlags(c.FLAG_WINDOW_RESIZABLE);
+    if (options.vsync) {
+        c.SetConfigFlags(c.FLAG_VSYNC_HINT);
+    }
+
     c.InitWindow(@as(c_int, @intFromFloat(options.size.w)), @as(c_int, @intFromFloat(options.size.h)), options.title);
-    const back = RaylibBackend{};
+
+    if (options.min_size) |min| {
+        c.SetWindowMinSize(@intFromFloat(min.w), @intFromFloat(min.h));
+    }
+    if (options.max_size) |max| {
+        c.SetWindowMaxSize(@intFromFloat(max.w), @intFromFloat(max.h));
+    }
+
+    var back = RaylibBackend{};
+    back.shader = c.LoadShaderFromMemory(vertexSource, fragSource);
     return back;
 }
 
@@ -52,9 +109,9 @@ pub fn sleep(self: *RaylibBackend, ns: u64) void {
     std.time.sleep(ns);
 }
 
-pub fn begin(_: *RaylibBackend, arena: std.mem.Allocator) void {
+pub fn begin(self: *RaylibBackend, arena: std.mem.Allocator) void {
+    self.arena = arena;
     c.BeginDrawing();
-    _ = arena;
 }
 
 pub fn end(_: *RaylibBackend) void {
@@ -80,44 +137,6 @@ pub fn contentScale(_: *RaylibBackend) f32 {
 pub fn renderGeometry(self: *RaylibBackend, texture: ?*anyopaque, vtx: []const dvui.Vertex, idx: []const u32) void {
     // TODO: scissor
     // TODO: texture
-    _ = self;
-    _ = texture;
-
-    const vertexSource =
-        \\#version 330
-        \\in vec3 vertexPosition;
-        //"in vec2 vertexTexCoord;            \n"
-        \\in vec4 vertexColor;
-        //"out vec2 fragTexCoord;             \n"
-        \\out vec4 fragColor;
-        \\uniform mat4 mvp;
-        \\void main()
-        \\{
-        //"    fragTexCoord = vertexTexCoord; \n"
-        \\    fragColor = vertexColor / 255.0;
-        //\\    fragColor.rgb *= fragColor.a;
-        \\    gl_Position = mvp*vec4(vertexPosition, 1.0);
-        //\\    gl_Position = vec4(vertexPosition, 1.0);
-        \\}
-    ;
-
-    const fragSource =
-        \\#version 330
-        //"in vec2 fragTexCoord;              \n"
-        \\in vec4 fragColor;
-        \\out vec4 finalColor;
-        //"uniform sampler2D texture0;        \n"
-        //"uniform vec4 colDiffuse;           \n"
-        //"void main()                        \n"
-        //"{                                  \n"
-        //"    vec4 texelColor = texture(texture0, fragTexCoord);   \n"
-        //"    finalColor = texelColor*colDiffuse*fragColor;        \n"
-        //"}                                  \n";
-        \\void main()
-        \\{
-        \\    finalColor = fragColor;
-        \\}
-    ;
 
     var mat: c.Matrix = undefined; // = c.GetCameraMatrix2D(camera);
     mat.m0 = 2.0 / @as(f32, @floatFromInt(c.GetRenderWidth()));
@@ -137,8 +156,7 @@ pub fn renderGeometry(self: *RaylibBackend, texture: ?*anyopaque, vtx: []const d
     mat.m14 = 0.0;
     mat.m15 = 1.0;
 
-    const shader = c.LoadShaderFromMemory(vertexSource, fragSource);
-
+    const shader = self.shader;
     c.SetShaderValueMatrix(shader, @intCast(shader.locs[c.RL_SHADER_LOC_MATRIX_MVP]), mat);
 
     const VAO = c.rlLoadVertexArray();
@@ -155,33 +173,61 @@ pub fn renderGeometry(self: *RaylibBackend, texture: ?*anyopaque, vtx: []const d
     c.rlSetVertexAttribute(@intCast(shader.locs[c.RL_SHADER_LOC_VERTEX_COLOR]), 4, c.RL_UNSIGNED_BYTE, false, @sizeOf(dvui.Vertex), @offsetOf(dvui.Vertex, "col"));
     c.rlEnableVertexAttribute(@intCast(shader.locs[c.RL_SHADER_LOC_VERTEX_COLOR]));
 
+    c.rlSetVertexAttribute(@intCast(shader.locs[c.RL_SHADER_LOC_VERTEX_TEXCOORD01]), 2, c.RL_FLOAT, false, @sizeOf(dvui.Vertex), @offsetOf(dvui.Vertex, "uv"));
+    c.rlEnableVertexAttribute(@intCast(shader.locs[c.RL_SHADER_LOC_VERTEX_TEXCOORD01]));
+
     c.rlEnableShader(shader.id);
 
+    const tex_coord_loc = c.GetShaderLocationAttrib(shader, "vertexTexCoord");
+    //std.debug.print("tex_coord_loc {d}\n", .{tex_coord_loc});
+    c.rlSetVertexAttribute(@intCast(tex_coord_loc), 2, c.RL_FLOAT, false, @sizeOf(dvui.Vertex), @offsetOf(dvui.Vertex, "uv"));
+    c.rlEnableVertexAttribute(@intCast(tex_coord_loc));
+
+    //c.glUniform1i(@intCast(shader.locs[c.RL_SHADER_LOC_MAP_DIFFUSE]), 0); // Active default sampler2D: texture0
+
+    if (texture) |tex| {
+        c.glUniform1i(c.GetShaderLocation(shader, "texture0"), 0);
+        c.glActiveTexture(c.GL_TEXTURE0);
+        //c.rlActiveTextureSlot(2);
+
+        const texid = @intFromPtr(tex);
+        c.glBindTexture(c.GL_TEXTURE_2D, @intCast(texid));
+        //c.rlEnableTexture(@intCast(texid));
+
+        c.glUniform1i(2, 1);
+    } else {
+        c.glUniform1i(2, 0);
+    }
+
     c.glDrawElements(c.GL_TRIANGLES, @intCast(idx.len), c.GL_UNSIGNED_INT, null);
+
+    c.rlDisableTexture();
 }
 
-pub fn textureCreate(self: *RaylibBackend, pixels: [*]u8, width: u32, height: u32) *anyopaque {
-    _ = self;
-    _ = pixels;
-    _ = width;
-    _ = height;
-    return @ptrFromInt(1);
+pub fn textureCreate(_: *RaylibBackend, pixels: [*]u8, width: u32, height: u32) *anyopaque {
+    // TODO: do we need to convert to premultiplied alpha?
+    const texid = c.rlLoadTexture(pixels, @intCast(width), @intCast(height), c.PIXELFORMAT_UNCOMPRESSED_R8G8B8A8, 1);
+    return @ptrFromInt(texid);
 }
 
 pub fn textureDestroy(_: *RaylibBackend, texture: *anyopaque) void {
-    _ = texture;
+    const texid = @intFromPtr(texture);
+    c.rlUnloadTexture(@intCast(texid));
 }
 
 pub fn clipboardText(_: *RaylibBackend) ![]const u8 {
-    return "TODO: clipboard";
+    return std.mem.sliceTo(c.GetClipboardText(), 0);
 }
 
-pub fn clipboardTextSet(_: *RaylibBackend, text: []const u8) !void {
-    _ = text;
+pub fn clipboardTextSet(self: *RaylibBackend, text: []const u8) !void {
+    //TODO can I free this memory??
+    const c_text = try self.arena.dupeZ(u8, text);
+    c.SetClipboardText(c_text.ptr);
 }
 
-pub fn openURL(_: *RaylibBackend, url: []const u8) !void {
-    _ = url;
+pub fn openURL(self: *RaylibBackend, url: []const u8) !void {
+    const c_url = try self.arena.dupeZ(u8, url);
+    c.SetClipboardText(c_url.ptr);
 }
 
 pub fn refresh(_: *RaylibBackend) void {}
