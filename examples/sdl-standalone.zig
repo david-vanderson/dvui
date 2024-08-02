62cc68ce738e6da20186a3f27387dc058d05cc4b
David Vanderson <david.vanderson@gmail.com>
2024-08-01 20:36:49 -0400

    sdl examples: adjust window titles

--- examples/sdl-standalone.zig
+++ examples/sdl-standalone.zig
@@ -23,7 +23,7 @@ pub fn main() !void {
         .size = .{ .w = 500.0, .h = 600.0 },
         .min_size = .{ .w = 250.0, .h = 350.0 },
         .vsync = vsync,
-        .title = "DVUI Standalone Example",
+        .title = "DVUI SDL Standalone Example",
         .icon = window_icon_png, // can also call setIconFromFileContent()
     });
     defer backend.deinit();
