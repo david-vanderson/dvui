3c3f107cf30ec61aabc3f0ee3e5737b3f0de8fa6
Robert Burnett <dr.slate2@gmail.com>
2024-08-13 12:03:54 -0500

    more experiments

--- src/structWidget.zig
+++ src/structWidget.zig
@@ -1,89 +1,136 @@
 const std = @import("std");
 const dvui = @import("dvui");
 
-pub fn IntFieldOptions(comptime T: type) type {
-    return ?struct {
-        min: T = std.math.intMin(T),
-        max: T = std.math.intMax(T),
-        widget_type: enum { value_box, slider } = .value_box,
-    };
-}
+//pub fn IntFieldOptions(comptime T: type) type {
+//    return struct {
+//        min: T = std.math.intMin(T),
+//        max: T = std.math.intMax(T),
+//        widget_type: enum { value_box, slider } = .value_box,
+//    };
+//}
+//
+pub const IntFieldOptions = struct {
+    widget_type: enum { value_box, slider } = .value_box,
+};
 
-pub fn intFieldWidget(src: std.builtin.SourceLocation, comptime T: type, result: *T, int_opt: IntFieldOptions(T), options: dvui.Options) !void {
+fn intFieldWidget(comptime src: std.builtin.SourceLocation, comptime T: type, int_opt: ?IntFieldOptions(T)) !T {
     _ = src; // autofix
-    _ = result; // autofix
     _ = int_opt; // autofix
-    _ = options; // autofix
 }
 
-pub fn FloatFieldOptions(comptime T: type) type {
-    return ?struct {
-        min: ?T = null,
-        max: ?T = null,
-        widget_type: enum { value_box, slider } = .value_box,
-    };
-}
+//fn FloatFieldOptions(comptime T: type) type {
+//    const Result = struct {
+//        min: ?T = null,
+//        max: ?T = null,
+//        widget_type: enum { value_box, slider } = .value_box,
+//    };
+//    @compileLog(T);
+//    @compileLog(Result);
+//    return Result;
+//}
 
-pub fn floatFieldWidget(src: std.builtin.SourceLocation, comptime T: type, result: *T, float_opt: FloatFieldOptions(T), options: dvui.Options) !void {
+pub const FloatFieldOptions = struct {
+    widget_type: enum { value_box, slider } = .value_box,
+};
+
+pub fn floatFieldWidget(comptime src: std.builtin.SourceLocation, comptime T: type, float_opt: ?FloatFieldOptions) !T {
     _ = float_opt; // autofix
     _ = src; // autofix
-    _ = result; // autofix
-    _ = options; // autofix
 }
 
-const EnumFieldOptions = ?struct {
+pub const EnumFieldOptions = struct {
     widget_type: enum { radio, dropdown } = .dropdown,
 };
 
-pub fn enumFieldWidget(src: std.builtin.SourceLocation, comptime T: type, result: *T, enum_opt: EnumFieldOptions, options: dvui.Options) !void {
+fn enumFieldWidget(comptime src: std.builtin.SourceLocation, comptime T: type, enum_opt: ?EnumFieldOptions) !T {
     _ = enum_opt; // autofix
     _ = src; // autofix
-    _ = result; // autofix
-    _ = options; // autofix
 }
 
-const BoolFieldOptions = ?struct {
+pub const BoolFieldOptions = struct {
     widget_type: enum { radio, checkbox, dropdown, toggle } = .checkbox,
 };
 
-pub fn boolFieldWidget(src: std.builtin.SourceLocation, comptime T: type, result: *T, bool_opt: BoolFieldOptions(T), options: dvui.Options) !void {
+fn boolFieldWidget(comptime src: std.builtin.SourceLocation, bool_opt: ?BoolFieldOptions) !bool {
     _ = bool_opt; // autofix
     _ = src; // autofix
-    _ = result; // autofix
-    _ = options; // autofix
 }
 
 pub fn StructOptions(comptime T: type) type {
     var fields: [@typeInfo(T).Struct.fields.len]std.builtin.Type.StructField = undefined;
     inline for (@typeInfo(T).Struct.fields, 0..) |field, i| {
-        fields[i] = switch (@typeInfo(field.type)) {
-            .Int => .{ .alignment = 1, .default_value = &null, .is_comptime = false, .name = field.name, .type = IntFieldOptions(field.type) },
-            .Float => .{ .alignment = 1, .default_value = &null, .is_comptime = false, .name = field.name, .type = FloatFieldOptions(field.type) },
-            .Bool => .{ .alignment = 1, .default_value = &null, .is_comptime = false, .name = field.name, .type = BoolFieldOptions },
-            .Enum => .{ .alignment = 1, .default_value = &null, .is_comptime = false, .name = field.name, .type = EnumFieldOptions },
-            .Struct => .{ .alignment = 1, .default_value = &(StructOptions(field.type){}), .is_comptime = false, .name = field.name, .type = StructOptions(field.type) },
-            else => @compileError("Invalid type for field"),
+        fields[i] = .{
+            .alignment = 1,
+            .default_value = null,
+            .is_comptime = false,
+            .name = field.name,
+            .type = switch (@typeInfo(field.type)) {
+                .Int => ?IntFieldOptions,
+                .Float => ?FloatFieldOptions,
+                .Bool => ?BoolFieldOptions,
+                .Enum => ?EnumFieldOptions,
+                .Struct => ?StructOptions(field.type),
+                else => @compileError("Invalid type for field"),
+            },
         };
     }
 
-    return @Type(std.builtin.Type{ .Struct = .{ .decls = &.{}, .fields = fields, .is_tuple = false, .layout = .auto } });
+    return @Type(.{
+        .Struct = .{
+            .decls = &.{},
+            .fields = &fields,
+            .is_tuple = false,
+            .layout = .auto,
+        },
+    });
 }
 
-pub fn structWidget(src: std.builtin.SourceLocation, comptime T: type, result: *T, struct_opts: StructOptions(T), options: dvui.Options) !void {
-    _ = options; // autofix
+/// For making sure all elements have unique ids
+const RecursionDepthTracker = struct {
+    depth: usize = 0,
+
+    const IdExtraType = @typeInfo(@TypeOf(dvui.Options.id_extra)).Optional.child;
+    const id_extra_num_bits = @typeInfo(IdExtraType).Int.bits;
+    const StructIdExtraType = u8;
+    const max_bits_per_struct = @typeInfo(StructIdExtraType).Int.bits;
+    const max_recursion_depth = id_extra_num_bits / max_bits_per_struct;
+
+    pub fn getIdExtra(self: RecursionDepthTracker, index: StructIdExtraType) IdExtraType {
+        return (1 << (self.depth * max_bits_per_struct)) + index;
+    }
+
+    pub fn descend(self: RecursionDepthTracker) RecursionDepthTracker {
+        if (self.depth < max_bits_per_struct) {
+            return .{ .depth = self.depth + 1 };
+        } else {
+            @compileError("Struct has too much recursion");
+        }
+    }
+};
+
+fn structFieldWidget(comptime src: std.builtin.SourceLocation, comptime T: type, comptime struct_opts: ?StructOptions(T), comptime depth: RecursionDepthTracker) !T {
     if (@typeInfo(T) != .Struct) @compileError("Input Type Must Be A Struct");
 
-    inline for (@typeInfo(T).Struct.fields, 0..) |field, i| {
+    var result: T = undefined;
+    inline for (@typeInfo(T).Struct.fields) |field| {
+        const options = if (struct_opts != null) @field(struct_opts.?, field.name) else null;
+        @compileLog(options);
 
         // zig fmt: off
-        switch (@typeInfo(field.type)) {
-            .Int => try intFieldWidget(src, field.type, &@field(result, field.name), @field(struct_opts, field.name), .{ .id_extra = i * 10000 }),
-            .Float, try floatFieldWidget(src, field.type, &@field(result, field.name), @field(struct_opts, field.name), .{ .id_extra = i * 10000 }),
-            .Bool => try boolFieldWidget(src, field.type, &@field(result, field.name), @field(struct_opts, field.name), .{ .id_extra = i * 10000 }),
-            .Enum => try enumFieldWidget(src, field.type, &@field(result, field.name), @field(struct_opts, field.name), .{ .id_extra = i * 10000 }),
-            .Struct => try structWidget(src, field.type, &@field(result, field.name), @field(struct_opts, field.name), .{ .id_extra = i * 10000 }),
+        @field(result, field.name) = switch (@typeInfo(field.type)) {
+            .Int => try intFieldWidget(src, field.type, options),
+            .Float, try floatFieldWidget(src, field.type, options),
+            .Bool => try boolFieldWidget(src, field.type, options),
+            .Enum => try enumFieldWidget(src, field.type, options),
+            .Struct => try structFieldWidget(src, field.type, options, depth.descend()),
             else => @compileError("Invalid type given"),
-        }
+        };
         // zig fmt: on
     }
+    return result;
+}
+
+pub fn structWidget(comptime src: std.builtin.SourceLocation, comptime T: type, struct_opts: ?StructOptions(T)) !T {
+    _ = struct_opts; // autofix
+    return try structFieldWidget(src, T, null, .{});
 }
