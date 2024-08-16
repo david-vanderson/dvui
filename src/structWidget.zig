4384b5bb8557e2ba7c7553d1c2a23e5383282ea8
Robert Burnett <dr.slate2@gmail.com>
2024-08-16 15:14:05 -0500

    bool widgets implemented

--- src/structWidget.zig
+++ src/structWidget.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const dvui = @import("dvui.zig");
 
 pub const IntFieldOptions = struct {
-    widget_type: enum { value_box, slider } = .value_box,
+    widget_type: enum { number_entry, slider } = .number_entry,
 };
 
 fn intFieldWidget(
@@ -10,14 +10,20 @@ fn intFieldWidget(
     comptime name: []const u8,
     comptime T: type,
     result: *T,
-    id_extra_range: IdExtraRange,
-    int_opt: IntFieldOptions,
+    id_allocator: IdAllocator,
+    comptime int_opt: IntFieldOptions,
 ) !void {
+    _ = name; // autofix
     _ = int_opt; // autofix
     _ = result; // autofix
-    var box = try dvui.box(src, .vertical, .{ .id_extra = id_extra_range.start });
+    var box = try dvui.box(src, .horizontal, .{ .id_extra = id_allocator.next() });
     defer box.deinit();
-    try dvui.label(src, "{s}: {s}", .{ name, @typeName(T) }, .{ .id_extra = id_extra_range.start + 1 });
+    //try dvui.label(src, "{s}: {s}", .{ name, @typeName(T) }, .{ .id_extra = id_allocator.next() });
+    //switch(int_opt.widget_type) {
+    //    .number_entry => {
+    //        result.* = try dvui.textEntryNumber(src, T, 
+    //    }
+    //}
 }
 
 pub const FloatFieldOptions = struct {
@@ -29,15 +35,15 @@ pub fn floatFieldWidget(
     comptime name: []const u8,
     comptime T: type,
     result: *T,
-    id_extra_range: IdExtraRange,
+    id_allocator: IdAllocator,
     float_opt: FloatFieldOptions,
 ) !void {
     _ = float_opt; // autofix
     _ = result; // autofix
     //
-    var box = try dvui.box(src, .vertical, .{ .id_extra = id_extra_range.start });
+    var box = try dvui.box(src, .vertical, .{ .id_extra = id_allocator.next() });
     defer box.deinit();
-    try dvui.label(src, "{s}: {s}", .{ name, @typeName(T) }, .{ .id_extra = id_extra_range.start + 1 });
+    try dvui.label(src, "{s}: {s}", .{ name, @typeName(T) }, .{ .id_extra = id_allocator.next() });
 }
 
 pub const EnumFieldOptions = struct {
@@ -49,33 +55,56 @@ fn enumFieldWidget(
     comptime name: []const u8,
     comptime T: type,
     result: *T,
-    id_extra_range: IdExtraRange,
+    id_allocator: IdAllocator,
     enum_opt: EnumFieldOptions,
 ) !void {
     _ = enum_opt; // autofix
     _ = result; // autofix
-    var box = try dvui.box(src, .vertical, .{ .id_extra = id_extra_range.start });
+    var box = try dvui.box(src, .vertical, .{ .id_extra = id_allocator.next() });
     defer box.deinit();
-    try dvui.label(src, "{s}: {s}", .{ name, @typeName(T) }, .{ .id_extra = id_extra_range.start + 1 });
+    try dvui.label(src, "{s}: {s}", .{ name, @typeName(T) }, .{ .id_extra = id_allocator.next() });
 }
 
 pub const BoolFieldOptions = struct {
-    widget_type: enum { radio, checkbox, dropdown, toggle } = .checkbox,
+    widget_type: enum { checkbox, dropdown, toggle } = .checkbox,
 };
 
 fn boolFieldWidget(
     comptime src: std.builtin.SourceLocation,
     comptime name: []const u8,
-    retult: *bool,
-    id_extra_range: IdExtraRange,
-    bool_opt: BoolFieldOptions,
+    result: *bool,
+    id_allocator: IdAllocator,
+    comptime bool_opt: BoolFieldOptions,
 ) !void {
-    _ = bool_opt; // autofix
-    _ = retult; // autofix
-    //
-    var box = try dvui.box(src, .vertical, .{ .id_extra = id_extra_range.start });
+    var box = try dvui.box(src, .horizontal, .{ .id_extra = id_allocator.next() });
     defer box.deinit();
-    try dvui.label(src, "{s}: bool", .{name}, .{ .id_extra = id_extra_range.start + 1 });
+
+    switch (bool_opt.widget_type) {
+        .checkbox => {
+            _ = try dvui.checkbox(src, result, name, .{ .id_extra = id_allocator.next() });
+        },
+        .dropdown => {
+            const entries = .{ "false", "true" };
+            var choice: usize = if (result.* == false) 0 else 1;
+            try dvui.labelNoFmt(src, name, .{ .id_extra = id_allocator.next() });
+            _ = try dvui.dropdown(src, &entries, &choice, .{ .id_extra = id_allocator.next() });
+            result.* = if (choice == 0) false else true;
+        },
+        .toggle => {
+            switch (result.*) {
+                true => {
+                    if (try dvui.button(src, name ++ " enabled", .{}, .{ .id_extra = id_allocator.next() })) {
+                        result.* = !result.*;
+                    }
+                },
+                false => {
+                    if (try dvui.button(src, name ++ " disabled", .{}, .{ .id_extra = id_allocator.next() })) {
+                        result.* = !result.*;
+                    }
+                },
+            }
+        },
+    }
 }
 
 pub fn StructFieldOptions(comptime T: type) type {
@@ -109,38 +138,14 @@ pub fn StructFieldOptions(comptime T: type) type {
 }
 
 const IdExtraType = u64; //@typeInfo(@TypeOf(dvui.Options.id_extra)).Optional.child;
+const IdAllocator = struct {
+    active_id: *IdExtraType,
 
-/// For making sure all elements have unique ids
-const RecursionDepthTracker = struct {
-    depth: u8 = 0,
-
-    const id_extra_num_bits = @typeInfo(IdExtraType).Int.bits;
-    const max_bits_per_struct = 8;
-    const max_recursion_depth = id_extra_num_bits / max_bits_per_struct;
-
-    pub fn getIdExtraRange(comptime self: RecursionDepthTracker, comptime field_widget_index: usize) IdExtraRange {
-        const start_offset = (field_widget_index * IdExtraRange.len) + 1;
-        const shift_amount: u6 = @intCast(self.depth * max_bits_per_struct);
-        const start: IdExtraType = start_offset << shift_amount;
-        return .{
-            .start = @intCast(start),
-            .end = @intCast(start + IdExtraRange.len),
-        };
+    pub fn next(self: @This()) IdExtraType {
+        const result = self.active_id.*;
+        self.active_id.* += 1;
+        return result;
     }
-
-    pub fn descend(comptime self: RecursionDepthTracker) RecursionDepthTracker {
-        if (self.depth < max_recursion_depth) {
-            return .{ .depth = self.depth + 1 };
-        } else {
-            @compileError("Struct has too much recursion");
-        }
-    }
-};
-
-const IdExtraRange = struct {
-    start: IdExtraType,
-    end: IdExtraType,
-    const len = 8;
 };
 
 fn structFieldWidget(
@@ -148,48 +153,42 @@ fn structFieldWidget(
     comptime name: []const u8,
     comptime T: type,
     result: *T,
+    id_allocator: IdAllocator,
     comptime struct_opts: StructFieldOptions(T),
-    comptime depth: RecursionDepthTracker,
 ) !void {
+    _ = name; // autofix
     if (@typeInfo(T) != .Struct) @compileError("Input Type Must Be A Struct");
 
-    if (depth.depth == 0 or try dvui.expander(
-        src,
-        name,
-        .{},
-        .{ .id_extra = depth.getIdExtraRange(0).start + 1 },
-    )) {
-        inline for (@typeInfo(T).Struct.fields, 1..) |field, i| {
-            const options = @field(struct_opts, field.name);
-            const result_ptr = &@field(result.*, field.name);
-            const id_extra_range = depth.getIdExtraRange(i);
+    inline for (@typeInfo(T).Struct.fields) |field| {
+        const options = @field(struct_opts, field.name);
+        const result_ptr = &@field(result.*, field.name);
 
-            switch (@typeInfo(field.type)) {
-                .Int => try intFieldWidget(src, field.name, field.type, result_ptr, id_extra_range, options),
-                .Float => try floatFieldWidget(src, field.name, field.type, result_ptr, id_extra_range, options),
-                .Bool => try boolFieldWidget(src, field.name, result_ptr, id_extra_range, options),
-                .Enum => try enumFieldWidget(src, field.name, field.type, result_ptr, id_extra_range, options),
-                .Struct => try structFieldWidget(src, field.name, field.type, result_ptr, options, depth.descend()),
-                //TODO more types
-                else => @compileError("Invalid type given"),
-            }
+        switch (@typeInfo(field.type)) {
+            .Int => try intFieldWidget(src, field.name, field.type, result_ptr, id_allocator, options),
+            .Float => try floatFieldWidget(src, field.name, field.type, result_ptr, id_allocator, options),
+            .Bool => try boolFieldWidget(src, field.name, result_ptr, id_allocator, options),
+            .Enum => try enumFieldWidget(src, field.name, field.type, result_ptr, id_allocator, options),
+            .Struct => try structFieldWidget(src, field.name, field.type, result_ptr, id_allocator, options),
+            else => @compileError("Invalid type given"),
         }
     }
 }
 
-pub fn structWidget(
+pub fn structEntry(
     comptime src: std.builtin.SourceLocation,
     comptime T: type,
     result: *T,
 ) !void {
-    try structWidgetEx(src, "", T, result, .{}, .{});
+    try structEntryEx(src, "", T, result, .{}, .{});
 }
 
-pub fn structWidgetEx(
+pub fn structEntryEx(
     comptime src: std.builtin.SourceLocation,
     comptime T: type,
     result: *T,
     comptime field_options: StructFieldOptions(T),
 ) !void {
-    try structFieldWidget(src, "", T, result, field_options, .{});
+    var starting_id_extra: IdExtraType = 1;
+    const id_allocator = IdAllocator{ .active_id = &starting_id_extra };
+    try structFieldWidget(src, "", T, result, id_allocator, field_options);
 }
