31a78c2809be81a0f83ce4b10e4e200b193f59e9
David Vanderson <david.vanderson@gmail.com>
2024-08-17 17:25:05 -0400

    dropdown: much more versatile api
    There is now a DropdownWidget:
    * arbitrary widgets in the non-dropped part
    * arbitrary widgets in the dropped part
    * non-dropped part can be anything, not restricted to one of the entries
    * entries are added lazily, and only if it's dropped
    
    One big win here is that even if you just want a list of text entries,
    you can produce the text on the fly from a datastructure.  So you don't
    have to construct a list of strings separately for it.

--- src/dvui.zig
+++ src/dvui.zig
@@ -3782,137 +3782,213 @@ pub fn animate(src: std.builtin.SourceLocation, kind: AnimateWidget.Kind, durati
     return ret;
 }
 
-pub var dropdown_defaults: Options = .{
-    .color_fill = .{ .name = .fill_control },
-    .margin = Rect.all(4),
-    .corner_radius = Rect.all(5),
-    .padding = Rect.all(4),
-    .background = true,
-    .name = "Dropdown",
-};
+pub const DropdownWidget = struct {
+    pub var defaults: Options = .{
+        .color_fill = .{ .name = .fill_control },
+        .margin = Rect.all(4),
+        .corner_radius = Rect.all(5),
+        .padding = Rect.all(4),
+        .background = true,
+        .name = "Dropdown",
+    };
 
-pub fn dropdown(src: std.builtin.SourceLocation, entries: []const []const u8, choice: *usize, opts: Options) !bool {
-    const options = dropdown_defaults.override(opts);
+    pub const InitOptions = struct {
+        label: ?[]const u8 = null,
+        selected_index: ?usize = null,
+    };
 
-    var m = try dvui.menu(@src(), .horizontal, options.wrapOuter());
-    defer m.deinit();
+    options: Options = undefined,
+    init_options: InitOptions = undefined,
+    menu: MenuWidget = undefined,
+    menuItem: MenuItemWidget = undefined,
+    drop: ?FloatingMenuWidget = null,
+    drop_first_frame: bool = false,
+    drop_mi: ?MenuItemWidget = null,
+    drop_mi_index: usize = 0,
+    drop_height: f32 = 0,
+    drop_adjust: f32 = undefined,
 
-    var b = MenuItemWidget.init(src, .{ .submenu = true }, options.wrapInner());
-    try b.install();
-    b.processEvents();
-    try b.drawBackground(.{ .focus_as_outline = true });
-    defer b.deinit();
+    pub fn init(src: std.builtin.SourceLocation, init_opts: InitOptions, opts: Options) DropdownWidget {
+        var self = DropdownWidget{};
+        self.options = defaults.override(opts);
+        self.init_options = init_opts;
+        self.menu = MenuWidget.init(src, .{ .dir = .horizontal }, self.options.wrapOuter());
+        self.drop_adjust = dvui.dataGet(null, self.menu.wd.id, "_drop_adjust", f32) orelse 0;
+        return self;
+    }
 
-    var hbox = try dvui.box(@src(), .horizontal, .{ .expand = .both });
-    defer hbox.deinit();
+    pub fn install(self: *DropdownWidget) !void {
+        try self.menu.install();
 
-    var lw = try LabelWidget.initNoFmt(@src(), entries[choice.*], options.strip().override(.{ .gravity_y = 0.5 }));
-    const lwrs = lw.wd.contentRectScale();
-    try lw.install();
-    try lw.draw();
-    lw.deinit();
-    try icon(@src(), "dropdown_triangle", entypo.chevron_small_down, options.strip().override(.{ .gravity_y = 0.5, .gravity_x = 1.0 }));
+        self.menuItem = MenuItemWidget.init(@src(), .{ .submenu = true }, self.options.wrapInner());
+        try self.menuItem.install();
+        self.menuItem.processEvents();
+        try self.menuItem.drawBackground(.{ .focus_as_outline = true });
 
-    var ret = false;
-    if (b.activeRect()) |r| {
-        const lw_rect = lwrs.r.scale(1 / windowNaturalScale());
-        var pop = FloatingMenuWidget.init(@src(), lw_rect, .{ .min_size_content = r.size() });
-        const first_frame = firstFrame(pop.wd.id);
+        if (self.init_options.label) |ll| {
+            var hbox = try dvui.box(@src(), .horizontal, .{ .expand = .both });
 
-        const s = pop.scale_val;
+            var lw = try LabelWidget.initNoFmt(@src(), ll, self.options.strip().override(.{ .gravity_y = 0.5 }));
+            try lw.install();
+            try lw.draw();
+            lw.deinit();
+            try icon(@src(), "dropdown_triangle", entypo.chevron_small_down, self.options.strip().override(.{ .gravity_y = 0.5, .gravity_x = 1.0 }));
 
-        // move popup to align first item with b
-        pop.initialRect.x -= MenuItemWidget.defaults.borderGet().x * s;
-        pop.initialRect.x -= MenuItemWidget.defaults.paddingGet().x * s;
-        pop.initialRect.y -= MenuItemWidget.defaults.borderGet().y * s;
-        pop.initialRect.y -= MenuItemWidget.defaults.paddingGet().y * s;
+            hbox.deinit();
+        }
+    }
 
-        pop.initialRect.x -= pop.options.borderGet().x;
-        pop.initialRect.x -= pop.options.paddingGet().x;
-        pop.initialRect.y -= pop.options.borderGet().y;
-        pop.initialRect.y -= pop.options.paddingGet().y;
+    pub fn dropped(self: *DropdownWidget) !bool {
+        if (self.drop != null) {
+            // protect against calling this multiple times
+            return true;
+        }
 
-        // move popup up so selected entry is aligned with b
-        const h = pop.wd.contentRect().inset(pop.options.borderGet()).inset(pop.options.paddingGet()).h;
-        pop.initialRect.y -= (h / @as(f32, @floatFromInt(entries.len))) * @as(f32, @floatFromInt(choice.*));
+        if (self.menuItem.activeRect()) |r| {
+            self.drop = FloatingMenuWidget.init(@src(), r, .{ .min_size_content = r.size() });
+            var drop = &self.drop.?;
+            self.drop_first_frame = firstFrame(drop.wd.id);
 
-        try pop.install();
-        defer pop.deinit();
+            const s = drop.scale_val;
 
-        // without this, if you trigger the dropdown with the keyboard and then
-        // move the mouse, the entries are highlighted but not focused
-        pop.menu.submenus_activated = true;
+            // move drop up to align first item
+            drop.initialRect.x -= drop.options.borderGet().x * s;
+            drop.initialRect.x -= drop.options.paddingGet().x * s;
+            drop.initialRect.y -= drop.options.borderGet().y * s;
+            drop.initialRect.y -= drop.options.paddingGet().y * s;
 
-        // only want a mouse-up to choose something if the mouse has moved in the popup
-        var eat_mouse_up = dataGet(null, pop.wd.id, "_eat_mouse_up", bool) orelse true;
-        var drag_scroll = dataGet(null, pop.wd.id, "_drag_scroll", bool) orelse false;
+            // move drop up so selected entry is aligned
+            drop.initialRect.y -= self.drop_adjust * s;
 
-        const pop_rs = pop.data().rectScale();
-        const scroll_rs = pop.scroll.data().contentRectScale();
-        const evts = events();
-        for (evts) |*e| {
-            if (drag_scroll and e.evt == .mouse and !e.evt.mouse.button.touch() and (e.evt.mouse.action == .motion or e.evt.mouse.action == .position)) {
-                if (e.evt.mouse.p.x >= scroll_rs.r.x and e.evt.mouse.p.x <= scroll_rs.r.x + scroll_rs.r.w and (e.evt.mouse.p.y <= scroll_rs.r.y or e.evt.mouse.p.y >= scroll_rs.r.y + scroll_rs.r.h)) {
-                    if (e.evt.mouse.action == .motion) {
-                        var scrolldrag = Event{ .evt = .{ .scroll_drag = .{
-                            .mouse_pt = e.evt.mouse.p,
-                            .screen_rect = pop.menu.data().rectScale().r,
-                            .capture_id = pop.wd.id,
-                        } } };
-                        pop.scroll.scroll.processEvent(&scrolldrag, true);
-                    } else if (e.evt.mouse.action == .position) {
-                        dvui.currentWindow().inject_motion_event = true;
+            try drop.install();
+
+            // without this, if you trigger the dropdown with the keyboard and then
+            // move the mouse, the entries are highlighted but not focused
+            drop.menu.submenus_activated = true;
+
+            // only want a mouse-up to choose something if the mouse has moved in the dropup
+            var eat_mouse_up = dataGet(null, drop.wd.id, "_eat_mouse_up", bool) orelse true;
+            var drag_scroll = dataGet(null, drop.wd.id, "_drag_scroll", bool) orelse false;
+
+            const drop_rs = drop.data().rectScale();
+            const scroll_rs = drop.scroll.data().contentRectScale();
+            const evts = events();
+            for (evts) |*e| {
+                if (drag_scroll and e.evt == .mouse and !e.evt.mouse.button.touch() and (e.evt.mouse.action == .motion or e.evt.mouse.action == .position)) {
+                    if (e.evt.mouse.p.x >= scroll_rs.r.x and e.evt.mouse.p.x <= scroll_rs.r.x + scroll_rs.r.w and (e.evt.mouse.p.y <= scroll_rs.r.y or e.evt.mouse.p.y >= scroll_rs.r.y + scroll_rs.r.h)) {
+                        if (e.evt.mouse.action == .motion) {
+                            var scrolldrag = Event{ .evt = .{ .scroll_drag = .{
+                                .mouse_pt = e.evt.mouse.p,
+                                .screen_rect = drop.menu.data().rectScale().r,
+                                .capture_id = drop.wd.id,
+                            } } };
+                            drop.scroll.scroll.processEvent(&scrolldrag, true);
+                        } else if (e.evt.mouse.action == .position) {
+                            dvui.currentWindow().inject_motion_event = true;
+                        }
                     }
                 }
-            }
 
-            if (!eventMatch(e, .{ .id = pop.data().id, .r = pop_rs.r }))
-                continue;
+                if (!eventMatch(e, .{ .id = drop.data().id, .r = drop_rs.r }))
+                    continue;
 
-            if (e.evt == .mouse) {
-                if (e.evt.mouse.action == .release and e.evt.mouse.button.pointer()) {
-                    if (eat_mouse_up) {
-                        e.handled = true;
-                        eat_mouse_up = false;
-                        dataSet(null, pop.wd.id, "_eat_mouse_up", eat_mouse_up);
-                    }
-                } else if (e.evt.mouse.action == .motion or (e.evt.mouse.action == .press and e.evt.mouse.button.pointer())) {
-                    if (eat_mouse_up) {
-                        eat_mouse_up = false;
-                        dataSet(null, pop.wd.id, "_eat_mouse_up", eat_mouse_up);
-                    }
+                if (e.evt == .mouse) {
+                    if (e.evt.mouse.action == .release and e.evt.mouse.button.pointer()) {
+                        if (eat_mouse_up) {
+                            e.handled = true;
+                            eat_mouse_up = false;
+                            dataSet(null, drop.wd.id, "_eat_mouse_up", eat_mouse_up);
+                        }
+                    } else if (e.evt.mouse.action == .motion or (e.evt.mouse.action == .press and e.evt.mouse.button.pointer())) {
+                        if (eat_mouse_up) {
+                            eat_mouse_up = false;
+                            dataSet(null, drop.wd.id, "_eat_mouse_up", eat_mouse_up);
+                        }
 
-                    if (!drag_scroll) {
-                        drag_scroll = true;
-                        dataSet(null, pop.wd.id, "_drag_scroll", drag_scroll);
+                        if (!drag_scroll) {
+                            drag_scroll = true;
+                            dataSet(null, drop.wd.id, "_drag_scroll", drag_scroll);
+                        }
                     }
                 }
             }
         }
 
-        for (entries, 0..) |_, i| {
-            var mi = try menuItem(@src(), .{}, .{ .id_extra = i, .expand = .horizontal });
-            if (first_frame and (i == choice.*)) {
-                focusWidget(mi.wd.id, null, null);
+        if (self.drop != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    pub fn addChoiceLabel(self: *DropdownWidget, label_text: []const u8) !bool {
+        var mi = try self.addChoice();
+        defer mi.deinit();
+
+        var opts = self.options.strip();
+        if (mi.show_active) {
+            opts = opts.override(dvui.themeGet().style_accent);
+        }
+
+        try labelNoFmt(@src(), label_text, opts);
+
+        if (mi.activeRect()) |_| {
+            dvui.menuGet().?.close();
+            return true;
+        }
+
+        return false;
+    }
+
+    pub fn addChoice(self: *DropdownWidget) !*MenuItemWidget {
+        // record how far down in our parent we would be
+        if (self.drop_mi) |*mi| {
+            self.drop_height += mi.data().min_size.h;
+        }
+
+        self.drop_mi = MenuItemWidget.init(@src(), .{}, .{ .id_extra = self.drop_mi_index, .expand = .horizontal });
+        try self.drop_mi.?.install();
+        self.drop_mi.?.processEvents();
+        try self.drop_mi.?.drawBackground(.{});
+
+        if (self.drop_first_frame) {
+            if (self.init_options.selected_index) |si| {
+                if (si == self.drop_mi_index) {
+                    focusWidget(self.drop_mi.?.wd.id, null, null);
+                    dvui.dataSet(null, self.menu.wd.id, "_drop_adjust", self.drop_height);
+                }
             }
-            defer mi.deinit();
+        }
+        self.drop_mi_index += 1;
 
-            var labelopts = options.strip();
+        return &self.drop_mi.?;
+    }
 
-            if (mi.show_active) {
-                labelopts = labelopts.override(dvui.themeGet().style_accent);
-            }
+    pub fn deinit(self: *DropdownWidget) void {
+        if (self.drop != null) {
+            self.drop.?.deinit();
+            self.drop = null;
+        }
+        self.menuItem.deinit();
+        self.menu.deinit();
+    }
+};
 
-            try labelNoFmt(@src(), entries[i], labelopts);
+pub fn dropdown(src: std.builtin.SourceLocation, entries: []const []const u8, choice: *usize, opts: Options) !bool {
+    var dd = dvui.DropdownWidget.init(src, .{ .selected_index = choice.*, .label = entries[choice.*] }, opts);
+    try dd.install();
 
-            if (mi.activeRect()) |_| {
+    var ret = false;
+    if (try dd.dropped()) {
+        for (entries, 0..) |e, i| {
+            if (try dd.addChoiceLabel(e)) {
                 choice.* = i;
                 ret = true;
-                dvui.menuGet().?.close();
             }
         }
     }
 
+    dd.deinit();
     return ret;
 }
 
