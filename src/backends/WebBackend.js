dcf1192852f615e909531511e31aaaaa7ad51851
David Vanderson <david.vanderson@gmail.com>
2024-09-25 21:39:38 -0400

    web: use texture linear interpolation
    This matches the sdl backend, and I'm pretty sure it's what we want.
    Although we might need an option in the future.

--- src/backends/WebBackend.js
+++ src/backends/WebBackend.js
@@ -189,11 +189,11 @@ function dvui(canvasId, wasmFile) {
 
             if (webgl2) {
                 gl.generateMipmap(gl.TEXTURE_2D);
-            } else {
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-            }
+	    }
+
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+            gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 
             return id;
         },
