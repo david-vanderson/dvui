59ba3e0ecf8810101f74b0fed36c150541d3151a
iacore <noreply+gpg-stub@1a-insec.net>
2024-10-03 21:19:38 +0000

    ++

--- src/Styler.zig
+++ src/Styler.zig
@@ -11,6 +11,14 @@ pub const Facet = union(enum) {
     fill: Color,
 };
 
+// list all used by the core widgets here
+pub const common_context = struct {
+    pub const body = "body";
+    pub const heading = "heading";
+    pub const caption = "caption";
+    pub const title = "title";
+};
+
 /// context: a string like "body text"
 pub fn style_me(a: std.mem.Allocator, widget: anytype, context: []const u8, comptime facet: std.meta.Tag(Facet)) !std.meta.TagPayload(Facet, facet) {
     _ = a;
