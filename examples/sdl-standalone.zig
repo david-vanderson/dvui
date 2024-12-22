6bedaf83b9a63a9caf6d195fe0ed479370f2fdf9
Deins <deinsegle@gmail.com>
2024-12-22 19:22:39 +0200

    SDL3 linux support

--- examples/sdl-standalone.zig
+++ examples/sdl-standalone.zig
@@ -28,6 +28,7 @@ pub fn main() !void {
         // on windows graphical apps have no console, so output goes to nowhere - attach it manually. related: https://github.com/ziglang/zig/issues/4196
         _ = winapi.AttachConsole(0xFFFFFFFF);
     }
+    std.log.info("SDL version: {}", .{Backend.getSDLVersion()});
 
     dvui.Examples.show_demo_window = show_demo;
 
