0c562b4a93b5bc3ffe7c77b6f05687417e9ea2c2
David Vanderson <david.vanderson@gmail.com>
2025-02-01 16:27:42 -0500

    fix compilation
    There is still a linker error when compiling web-test with ReleaseFast
    about ldexp, but this rolls back the fix I had for that that broke
    everything else.

--- src/stb/stb_image_write_impl.c
+++ src/stb/stb_image_write_impl.c
@@ -6,11 +6,5 @@
 #define STBI_WRITE_NO_STDIO
 #define STBIW_WINDOWS_UTF8
 
-extern double dvui_c_ldexp(double x, int n);
-/* this should be in stb_image_libc.c like the rest, but then I get a linker error in ReleaseFast */
-static double ldexp(double x, int n) {
-    return dvui_c_ldexp(x, n);
-}
-
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
