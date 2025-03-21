502940f3b80aa0ae1ba36126b957c222d9d85482
RedPhoenixQ <steve.dark.03@gmail.com>
2025-03-22 00:11:41 +0100

    fix: Add null terminator when length reaches 0 in TextEntryWidget
    When deleting next or previous word, the null terminator was only added
    if length was greater than 0. This caused a flash of the content were
    `len` is correct for one frame, and then the null terminator reset the
    length on the next frame.

--- src/widgets/TextEntryWidget.zig
+++ src/widgets/TextEntryWidget.zig
@@ -660,7 +660,7 @@ pub fn processEvent(self: *TextEntryWidget, e: *Event, bubbling: bool) void {
                             // delete from sel.cursor to oldcur
                             std.mem.copyForwards(u8, self.text[sel.cursor..], self.text[oldcur..self.len]);
                             self.len -= (oldcur - sel.cursor);
-                            if (self.len > 0) {
+                            if (self.len >= 0) {
                                 self.text[self.len] = 0;
                             }
                             sel.end = sel.cursor;
@@ -719,7 +719,7 @@ pub fn processEvent(self: *TextEntryWidget, e: *Event, bubbling: bool) void {
                             // delete from oldcur to sel.cursor
                             std.mem.copyForwards(u8, self.text[oldcur..], self.text[sel.cursor..self.len]);
                             self.len -= (sel.cursor - oldcur);
-                            if (self.len > 0) {
+                            if (self.len >= 0) {
                                 self.text[self.len] = 0;
                             }
                             self.text_changed = (sel.cursor != oldcur);
