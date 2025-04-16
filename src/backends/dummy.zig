fb181d745d8790febb3bd2d1dff2e341ad090014
RedPhoenixQ <steve.dark.03@gmail.com>
2025-04-17 00:52:50 +0200

    dummy-backend: handle textures actual texture data

--- src/backends/dummy.zig
+++ src/backends/dummy.zig
@@ -84,31 +84,34 @@ pub fn drawClippedTriangles(_: *DummyBackend, _: ?dvui.Texture, _: []const dvui.
 
 /// Create a texture from the given pixels in RGBA.  The returned
 /// pointer is what will later be passed to drawClippedTriangles.
-pub fn textureCreate(self: *DummyBackend, _: [*]u8, width: u32, height: u32, _: dvui.enums.TextureInterpolation) dvui.Texture {
+pub fn textureCreate(self: *DummyBackend, pixels: [*]u8, width: u32, height: u32, _: dvui.enums.TextureInterpolation) dvui.Texture {
+    const new_pixels = self.allocator.dupe(u8, pixels[0 .. width * height * 4]) catch @panic("Couldn't create texture: OOM");
     return .{
         .width = width,
         .height = height,
-        .ptr = self, // arbitrary pointer
+        .ptr = new_pixels.ptr,
     };
 }
 
 /// Create a texture that can be rendered to with renderTarget().  The
 /// returned pointer is what will later be passed to drawClippedTriangles.
-pub fn textureCreateTarget(self: *DummyBackend, width: u32, height: u32, _: dvui.enums.TextureInterpolation) error{ OutOfMemory, TextureCreate }!dvui.Texture {
-    return .{
-        .width = width,
-        .height = height,
-        .ptr = self,
-    };
+pub fn textureCreateTarget(_: *DummyBackend, _: u32, _: u32, _: dvui.enums.TextureInterpolation) error{ OutOfMemory, TextureCreate }!dvui.Texture {
+    return error.TextureCreate;
 }
 
 /// Read pixel data (RGBA) from texture into pixel.
-pub fn textureRead(_: *DummyBackend, _: dvui.Texture, _: [*]u8) error{TextureRead}!void {}
+pub fn textureRead(_: *DummyBackend, texture: dvui.Texture, pixels: [*]u8) error{TextureRead}!void {
+    const ptr: [*]const u8 = @ptrCast(texture.ptr);
+    @memcpy(pixels, ptr[0..(texture.width * texture.height * 4)]);
+}
 
 /// Destroy texture that was previously made with textureCreate() or
 /// textureCreateTarget().  After this call, this texture pointer will not
 /// be used by dvui.
-pub fn textureDestroy(_: *DummyBackend, _: dvui.Texture) void {}
+pub fn textureDestroy(self: *DummyBackend, texture: dvui.Texture) void {
+    const ptr: [*]const u8 = @ptrCast(texture.ptr);
+    self.allocator.free(ptr[0..(texture.width * texture.height * 4)]);
+}
 
 /// Render future drawClippedTriangles() to the passed texture (or screen
 /// if null).
