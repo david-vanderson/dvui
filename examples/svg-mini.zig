12995660c8574ec0ba5b6470305d27428eb05ba7
Aurélien Cibrario <aurelien.cibrario@gmail.com>
2025-04-26 20:35:26 +0200

    svg backend : fix memory leak

--- examples/svg-mini.zig
+++ examples/svg-mini.zig
@@ -3,20 +3,21 @@ comptime {
     std.debug.assert(@hasDecl(Backend, "SvgBackend"));
 }
 
-var gpa_instance = std.heap.GeneralPurposeAllocator(.{}){};
-const gpa = gpa_instance.allocator();
-
 const zig_icon = @embedFile("zig-favicon.png");
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
     var backend = try Backend.initWindow(.{
-        .allocator = gpa,
+        .allocator = allocator,
         .size = .{ .w = 800.0, .h = 600.0 },
     });
     defer backend.deinit();
 
     // init dvui Window (In that case only prepare the svg backend to dump files)
-    var win = try dvui.Window.init(@src(), gpa, backend.backend(), .{});
+    var win = try dvui.Window.init(@src(), allocator, backend.backend(), .{});
     defer win.deinit();
 
     dvui.Examples.show_demo_window = true;
