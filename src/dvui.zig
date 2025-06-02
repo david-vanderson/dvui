61da8b4cbefcec56bc517d83ac011f800bef1de4
RedPhoenixQ <steve.dark.03@gmail.com>
2025-06-02 03:08:44 +0200

    arena: add shrinking arena allocator to prevent capacity leak
    Before when using the `std.heap.ArenaAllocator` the capacity would never
    shrinking as the `.retain_capacity` option was passed to the arena. We
    could not easily use `.retain_with_limit` before as the reasonable limit
    would change per application or view in the application.
    
    To address this a wrapper around the `std.heap.ArenaAllocator` was
    created that keeps track of the peak memory usage before the reset and
    retains only that much, with some padding. This allows the arena to
    shrinking over time but still adapt and be as fast as before on "static"
    views.
    
    I first tried to calculate the memory used based on the capacity and
    `end_index` of the `std.heap.ArenaAllocator`. This was unreliable as
    memory could have been freed before the check, like in the plot example,
    causing the needed capacity to be freed and reallocated every frame.

--- src/dvui.zig
+++ src/dvui.zig
@@ -91,6 +91,7 @@ pub const StructFieldOptions = se.StructFieldOptions;
 pub const enums = @import("enums.zig");
 pub const easing = @import("easing.zig");
 pub const testing = @import("testing.zig");
+pub const ShrinkingArenaAllocator = @import("shrinking_arena_allocator.zig");
 
 pub const wasm = (builtin.target.cpu.arch == .wasm32 or builtin.target.cpu.arch == .wasm64);
 pub const useFreeType = !wasm;
