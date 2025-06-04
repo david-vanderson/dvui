fcf61ee4c9fc727c2ff124031e7efec858672873
RedPhoenixQ <steve.dark.03@gmail.com>
2025-06-04 21:17:50 +0200

    debugFontAtlas: use renderTexture to avoid manual triangle handling
    This removes all the manual triangle handling, as all the function did
    was to render the font atlas textures. This also allows for the removal
    of the `RenderCommand` for debugging font atlases as `renderTexture`
    handles that for us.

--- src/Window.zig
+++ src/Window.zig
@@ -1167,9 +1167,6 @@ pub fn renderCommands(self: *Self, queue: std.ArrayList(dvui.RenderCommand)) !vo
             .text => |t| {
                 try dvui.renderText(t);
             },
-            .debug_font_atlases => |t| {
-                try dvui.debugRenderFontAtlases(t.rs, t.color);
-            },
             .texture => |t| {
                 try dvui.renderTexture(t.tex, t.rs, t.opts);
             },
