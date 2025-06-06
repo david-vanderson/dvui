bb76f28b854996d7c094cc4626d1504b884dd7fc
foxnne <foxnne@gmail.com>
2025-06-06 07:42:20 -0500

    IconWidget: Initial color set to white to prevent final fill color being multiplied with text color

--- src/widgets/IconWidget.zig
+++ src/widgets/IconWidget.zig
@@ -49,7 +49,7 @@ pub fn matchEvent(self: *IconWidget, e: *dvui.Event) bool {
 
 pub fn draw(self: *IconWidget) !void {
     const rs = self.wd.parent.screenRectScale(self.wd.contentRect());
-    try dvui.renderIcon(self.name, self.tvg_bytes, rs, .{ .rotation = self.wd.options.rotationGet(), .colormod = self.wd.options.color(.text) }, self.icon_opts);
+    try dvui.renderIcon(self.name, self.tvg_bytes, rs, .{ .rotation = self.wd.options.rotationGet(), .colormod = .white }, self.icon_opts);
 }
 
 pub fn deinit(self: *IconWidget) void {
