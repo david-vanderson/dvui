c2fd87327ce0fc7e8ed75f273cae7f4ddd644a9a
RedPhoenixQ <steve.dark.03@gmail.com>
2025-06-28 02:39:18 +0200

    scroll: translate to scroll wheel "ticks" and use a common multiplier
    This makes scrolling uniform for all backends on my Windows machine.
    More testing needs to be done on other platforms.

--- src/backends/web.zig
+++ src/backends/web.zig
@@ -284,7 +284,7 @@ fn add_event_raw(w: *dvui.Window, which: u8, int1: u32, int2: u32, float1: f32, 
         1 => _ = try w.addEventMouseMotion(.{ .x = float1, .y = float2 }),
         2 => _ = try w.addEventMouseButton(buttonFromJS(int1), .press),
         3 => _ = try w.addEventMouseButton(buttonFromJS(int1), .release),
-        4 => _ = try w.addEventMouseWheel(if (float1 > 0) -20 else 20, if (int1 > 0) .vertical else .horizontal),
+        4 => _ = try w.addEventMouseWheel(float1 * dvui.scroll_speed, if (int1 > 0) .vertical else .horizontal),
         5 => {
             const str = @as([*]u8, @ptrFromInt(int1))[0..int2];
             _ = try w.addEventKey(.{
