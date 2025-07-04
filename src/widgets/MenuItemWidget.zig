b0c95b763ecd47f85f880767492cddfe6e0963f3
RedPhoenixQ <steve.dark.03@gmail.com>
2025-07-05 00:22:20 +0200

    fix: update menu item last focused test

--- src/widgets/MenuItemWidget.zig
+++ src/widgets/MenuItemWidget.zig
@@ -265,20 +265,20 @@ test "menuItem click sets last_focused_id_this_frame" {
     defer t.deinit();
 
     const fns = struct {
-        var last_focused_id_set: dvui.WidgetId = .zero;
+        var last_focused_id_set: ?dvui.WidgetId = null;
 
         fn frame() !dvui.App.Result {
             var m = dvui.menu(@src(), .vertical, .{ .padding = .all(10), .tag = "menu" });
             defer m.deinit();
 
-            const last_focused = dvui.lastFocusedIdInFrame(null);
+            const last_focused = dvui.lastFocusedIdInFrame();
 
             if (dvui.menuItemLabel(@src(), "item 1", .{}, .{ .tag = "item 1" })) |_| {
                 dvui.focusWidget(m.data().id, null, null);
             }
             _ = dvui.menuItemLabel(@src(), "item 2", .{}, .{ .tag = "item 2" });
 
-            last_focused_id_set = dvui.lastFocusedIdInFrame(last_focused);
+            last_focused_id_set = dvui.lastFocusedIdInFrameFrom(last_focused);
 
             return .ok;
         }
