9321b1fef84c0ed801a66a1cdfd9061a41753d10
David Vanderson <david.vanderson@gmail.com>
2025-08-29 15:26:56 -0400

    updates for zig 0.15.1
    * only enough to get past the initial dependency issues
    * now starting to get the actual code errors

--- build.zig
+++ build.zig
@@ -122,8 +122,10 @@ pub fn build(b: *std.Build) !void {
         // Use customized index.html
         const add_doc_logo = b.addExecutable(.{
             .name = "addDocLogo",
-            .root_source_file = b.path("docs/add_doc_logo.zig"),
-            .target = b.graph.host,
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("docs/add_doc_logo.zig"),
+                .target = b.graph.host,
+            }),
         });
         const run_add_logo = b.addRunArtifact(add_doc_logo);
         run_add_logo.addFileArg(b.path("docs/index.html"));
@@ -609,7 +611,7 @@ fn addExample(
     mod.addImport("dvui", example_opts.dvui_mod);
     mod.addImport(example_opts.backend_name, example_opts.backend_mod);
 
-    const exe = b.addExecutable(.{ .name = name, .root_module = mod, .use_lld = opts.use_lld });
+    const exe = b.addExecutable(.{ .name = name, .root_module = mod, .use_lld = opts.use_lld, .use_llvm = true });
     if (opts.check_step) |step| {
         step.dependOn(&exe.step);
     }
@@ -653,11 +655,13 @@ fn addWebExample(
 
     const exeOptions: std.Build.ExecutableOptions = .{
         .name = "web",
-        .root_source_file = file,
-        .target = opts.target,
-        .optimize = opts.optimize,
-        .link_libc = false,
-        .strip = if (opts.optimize == .ReleaseFast or opts.optimize == .ReleaseSmall) true else false,
+        .root_module = b.createModule(.{
+            .root_source_file = file,
+            .target = opts.target,
+            .optimize = opts.optimize,
+            .link_libc = false,
+            .strip = if (opts.optimize == .ReleaseFast or opts.optimize == .ReleaseSmall) true else false,
+        }),
     };
     const web_test = b.addExecutable(exeOptions);
     web_test.entry = .disabled;
@@ -678,8 +682,10 @@ fn addWebExample(
 
     const cb = b.addExecutable(.{
         .name = "cacheBuster",
-        .root_source_file = b.path("src/cacheBuster.zig"),
-        .target = b.graph.host,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/cacheBuster.zig"),
+            .target = b.graph.host,
+        }),
     });
     const cb_run = b.addRunArtifact(cb);
     cb_run.addFileArg(b.path("src/backends/index.html"));
