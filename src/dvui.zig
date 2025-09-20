019d076bcddf6d3bb5b72da6fd8fd09e55117a68
rasp2006 <78358571+rasp2006@users.noreply.github.com>
2025-09-20 09:00:07 +0500

    Refactor
    clickedEx no longer depends on pointer method, clicked works as intended

--- src/dvui.zig
+++ src/dvui.zig
@@ -3500,9 +3500,6 @@ pub const ClickOptions = struct {
     ///
     /// Defaults to the border rect of the `WidgetData`
     rect: ?Rect.Physical = null,
-
-    /// Which mouse buttons to react to.
-    buttons: enum { pointer, any } = .pointer,
 };
 
 pub fn clickedEx(wd: *const WidgetData, opts: ClickOptions) ?Event.EventTypes {
@@ -3520,13 +3517,13 @@ pub fn clickedEx(wd: *const WidgetData, opts: ClickOptions) ?Event.EventTypes {
 
                     // focus this widget for events after this one (starting with e.num)
                     dvui.focusWidget(wd.id, null, e.num);
-                } else if (me.action == .press and (if (opts.buttons == .pointer) me.button.pointer() else true)) {
+                } else if (me.action == .press and me.button != enums.Button.none) {
                     e.handle(@src(), wd);
                     dvui.captureMouse(wd, e.num);
 
                     // for touch events, we want to cancel our click if a drag is started
                     dvui.dragPreStart(me.p, .{});
-                } else if (me.action == .release and (if (opts.buttons == .pointer) me.button.pointer() else true)) {
+                } else if (me.action == .release and me.button != enums.Button.none) {
                     // mouse button was released, do we still have mouse capture?
                     if (dvui.captured(wd.id)) {
                         e.handle(@src(), wd);
@@ -3587,11 +3584,21 @@ pub fn clickedEx(wd: *const WidgetData, opts: ClickOptions) ?Event.EventTypes {
 
 /// Handles all events needed for clicking behaviour, used by `ButtonWidget`.
 pub fn clicked(wd: *const WidgetData, opts: ClickOptions) bool {
-    if (clickedEx(wd, opts)) |_| {
-        return true;
-    }
+    const click_evt = clickedEx(wd, opts) orelse return false;
 
-    return false;
+    switch (click_evt) {
+        .mouse => |mouse_evt| {
+            // now only default clicked depends on the pointer method
+            return mouse_evt.button.pointer();
+        },
+        // key was already checked
+        .key => |_| {
+            return true;
+        },
+        else => {
+            return false;
+        },
+    }
 }
 
 /// Animation state - see `animation` and `animationGet`.
