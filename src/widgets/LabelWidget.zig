fb29003f2d6bb4f131c4e69727eefba73195e683
BrotherJ <97724090+br07h3rj@users.noreply.github.com>
2025-10-08 16:37:12 -0400

    Update LabelWidget.zig
    Remove independent ellipsize variable, replaced with ellipsized field.

--- src/widgets/LabelWidget.zig
+++ src/widgets/LabelWidget.zig
@@ -126,12 +126,10 @@ pub fn draw(self: *LabelWidget) void {
         var lineRect = dvui.placeIn(self.data().contentRect(), tsize, .none, label_gravity);
 
         const ellip = "...";
-        var ellipsize = false;
         self.ellipsized = false;
         // give ourselves a fraction of a pixel extra for floating point innacurracies:
         // - a lot of times the content Rect is sized based on the text width
         if (self.init_options.ellipsize and tsize.w > (self.data().contentRect().w + 0.001)) {
-            ellipsize = true;
             self.ellipsized = true;
             const esize = self.data().options.fontGet().textSize(ellip);
             var endi: usize = 0;
@@ -152,7 +150,7 @@ pub fn draw(self: *LabelWidget) void {
             dvui.logError(@src(), err, "Failed to render text: {s}", .{line});
         };
 
-        if (ellipsize) {
+        if (self.ellipsized) {
             rs.r.x += liners.r.w;
             dvui.renderText(.{
                 .font = self.data().options.fontGet(),
