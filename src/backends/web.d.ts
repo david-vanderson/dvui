f639fb3db8965ef78d376aed3122416b3f7bd8e8
Michael Belousov <michael.belousov98@gmail.com>
2026-01-25 14:05:18 -0500

    export Dvui and expose more methods and the constructor via types

--- src/backends/web.d.ts
+++ src/backends/web.d.ts
@@ -8,9 +8,27 @@ interface DvuiOptions {
     wasmImportsExtra: WebAssembly.Imports | undefined
 }
 
+/** instantiates a Dvui object, sets the WASM, and starts rendering */
 export function dvui(
     canvas: string | HTMLCanvasElement,
     wasmRef: WasmArg
-): Dvui;
+): Promise<Dvui>;
 
-export interface Dvui {}
+/**
+ * if you need more control over the initialization, you can instantiate a Dvui instance yourself
+ * and start it later
+ */
+export interface Dvui {
+    setInstance(inst: WebAssembly.Instance): void;
+    /** can be an element or a string selector to query for an element */
+    setCanvas(canvas: string | HTMLCanvasElement): void;
+    //render(): void;
+    stop(): void;
+    run(): void;
+    restart(): void;
+    requestRender(): void;
+    init(): void;
+    imports: WebAssembly.ModuleImports;
+}
+
+export const Dvui: new() => Dvui;
