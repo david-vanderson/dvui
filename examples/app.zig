50078a45a15f5e4f14c620dfa29d819ce017397e
David Vanderson <david.vanderson@gmail.com>
2026-02-22 13:33:02 -0500

    example: adjust naming to zig standard
    From Codotaku

--- examples/app.zig
+++ examples/app.zig
@@ -21,9 +21,9 @@ pub const dvui_app: dvui.App = .{
             },
         },
     },
-    .frameFn = AppFrame,
-    .initFn = AppInit,
-    .deinitFn = AppDeinit,
+    .frameFn = appFrame,
+    .initFn = appInit,
+    .deinitFn = appDeinit,
 };
 pub const main = dvui.App.main;
 pub const panic = dvui.App.panic;
@@ -40,7 +40,7 @@ var warn_on_quit_closing: bool = false;
 
 // Runs before the first frame, after backend and dvui.Window.init()
 // - runs between win.begin()/win.end()
-pub fn AppInit(win: *dvui.Window) !void {
+pub fn appInit(win: *dvui.Window) !void {
     orig_content_scale = win.content_scale;
 
     // Add your own bundled font files...:
@@ -58,10 +58,10 @@ pub fn AppInit(win: *dvui.Window) !void {
 }
 
 // Run as app is shutting down before dvui.Window.deinit()
-pub fn AppDeinit() void {}
+pub fn appDeinit() void {}
 
 // Run each frame to do normal UI
-pub fn AppFrame() !dvui.App.Result {
+pub fn appFrame() !dvui.App.Result {
     return frame();
 }
 
