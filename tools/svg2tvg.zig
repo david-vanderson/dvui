4e4943838a506d910a534d6b1faf16d75ba21028
David Vanderson <david.vanderson@gmail.com>
2026-04-30 13:54:12 -0400

    tools: svg2tvg: update for zig 0.16.0

--- tools/svg2tvg.zig
+++ tools/svg2tvg.zig
@@ -1,14 +1,8 @@
 const std = @import("std");
 const svg2tvg = @import("svg2tvg");
 
-pub fn main() !void {
-    return impl();
-}
-
-fn impl() !void {
-    const gpa = std.heap.smp_allocator;
-    var args = try std.process.argsWithAllocator(gpa);
-    defer args.deinit();
+pub fn main(main_init: std.process.Init) !void {
+    var args = main_init.minimal.args.iterate();
 
     _ = args.next();
     const input_path = args.next() orelse {
@@ -25,23 +19,24 @@ fn impl() !void {
     errdefer {
         var path_buf: [1024]u8 = undefined;
         std.debug.print("error: input_path='{s}' output_path='{s}'", .{ input_path, output_path });
-        const cwd = std.fs.cwd().realpath(".", &path_buf) catch "CWD TOO LONG";
+        const n = std.Io.Dir.cwd().realPathFile(main_init.io, ".", &path_buf) catch 0;
+        const cwd: []const u8 = if (n > 0) path_buf[0..n] else "CWD TOO LONG";
         std.debug.print(" cwd='{s}'\n", .{cwd});
     }
 
-    const input_file = try std.fs.cwd().openFile(input_path, .{});
-    defer input_file.close();
+    const input_file = try std.Io.Dir.cwd().openFile(main_init.io, input_path, .{});
+    defer input_file.close(main_init.io);
 
-    const output_file = try std.fs.cwd().createFile(output_path, .{});
-    defer output_file.close();
+    const output_file = try std.Io.Dir.cwd().createFile(main_init.io, output_path, .{});
+    defer output_file.close(main_init.io);
 
     var buf: [8192]u8 = undefined;
-    //var reader = input_file.reader(read_buf);
 
-    const svg_bytes = try input_file.readToEndAlloc(gpa, 1 << 16);
-    const tvg_bytes = try svg2tvg.tvg_from_svg(gpa, svg_bytes, .{});
+    var file_reader = input_file.reader(main_init.io, &.{});
+    const svg_bytes = try file_reader.interface.allocRemaining(main_init.arena.allocator(), .limited(1 << 16));
+    const tvg_bytes = try svg2tvg.tvg_from_svg(main_init.arena.allocator(), svg_bytes, .{});
 
-    var writer = output_file.writer(&buf);
+    var writer = output_file.writer(main_init.io, &buf);
     try writer.interface.writeAll(tvg_bytes);
     try writer.interface.flush();
     // REPORTME: would be nice to have a
