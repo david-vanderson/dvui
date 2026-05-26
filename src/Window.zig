51ddf54726f4789f30d883b31a392688f51dc421
David Vanderson <david.vanderson@gmail.com>
2026-05-26 12:01:41 -0400

    Window.begin: use reasonable frame elapsed if time goes backwards
    We currently enforce that frame_time_ns doesn't go backwards.  If the backend
    shifts time backwards on us, then we stay on the same frame_time_ns.
    
    In that case, we have to use some value for updating animations.  We had 1us,
    but that effectively means animations will never finish.  Instead use 10ms.
    
    part of #879

--- src/Window.zig
+++ src/Window.zig
@@ -952,7 +952,10 @@ pub fn begin(
         _ = dvui.backend.c.SDL_ShowWindow(self.backend.impl.window);
     }
 
-    var micros_since_last: u32 = 1;
+    // If time_ns jumps backward, then we will stay on the current
+    // frame_time_ns.  In that case we use a dummy value (10ms) for updating
+    // animations and `secondsSinceLastFrame`.
+    var micros_since_last: u32 = 10_000;
     if (time_ns > self.frame_time_ns) {
         // enforce monotinicity
         var nanos_since_last = time_ns - self.frame_time_ns;
