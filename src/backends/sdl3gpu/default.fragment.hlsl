8fbaee1c0747e1de1f2075e9d2e1e2341d8f868f
David Vanderson <david.vanderson@gmail.com>
2026-05-30 21:15:17 -0400

    sdl3gpu: fix uv wrapping
    fixes #880

--- src/backends/sdl3gpu/default.fragment.hlsl
+++ src/backends/sdl3gpu/default.fragment.hlsl
@@ -10,11 +10,6 @@ SamplerState Sampler0 : register(s0, space2);
 PixelOutput main(PSInput input)
 {
     PixelOutput o;
-    if(input.texcoord.x < 0 || input.texcoord.x > 1 || input.texcoord.y < 0 || input.texcoord.y > 1)
-    {
-        o.Color = input.color;
-        return o;
-    }
     float4 sampled = Texture0.Sample(Sampler0, input.texcoord);
     o.Color = sampled * input.color;
     return o;
